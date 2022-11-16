3b1f9d8e47d48ca197d7a4a58dbbc779316be5d9
WinPlay02 <winplay02_gh@woberlaender.de>
2022-11-16 12:39:09 +0100

    feat: Create table from file: read_csv and read_json (#164)
    Closes #145.
    
    ### Summary of Changes
    
    Added functions: read_csv and read_json
    Added Tests: valid and invalid inputs
    
    Co-authored-by: WinPlay02 <WinPlay02@users.noreply.github.com>
    Co-authored-by: Lars Reimann <mail@larsreimann.com>
    Co-authored-by: lars-reimann <lars-reimann@users.noreply.github.com>

--- Runtime/safe-ds/safe_ds/data/_table.py
+++ Runtime/safe-ds/safe_ds/data/_table.py
@@ -1,6 +1,70 @@
+from __future__ import annotations
+
 import pandas as pd
 
 
 class Table:
     def __init__(self, data: pd.DataFrame):
         self._data: pd.DataFrame = data
+
+    @staticmethod
+    def from_json(path: str) -> Table:
+        """
+        Reads data from a JSON file into a Table
+
+        Parameters
+        ----------
+        path : str
+            Path to the file as String
+
+        Returns
+        -------
+        table : Table
+            The Table read from the file
+
+        Raises
+        ------
+        FileNotFoundError
+            If the specified file does not exist
+        ValueError
+            If the file could not be read
+        """
+
+        try:
+            return Table(pd.read_json(path))
+        except FileNotFoundError as exception:
+            raise FileNotFoundError(f'File "{path}" does not exist') from exception
+        except Exception as exception:
+            raise ValueError(
+                f'Could not read file from "{path}" as JSON'
+            ) from exception
+
+    @staticmethod
+    def from_csv(path: str) -> Table:
+        """
+        Reads data from a CSV file into a Table.
+
+        Parameters
+        ----------
+        path : str
+            Path to the file as String
+
+        Returns
+        -------
+        table : Table
+            The Table read from the file
+
+        Raises
+        ------
+        FileNotFoundError
+            If the specified file does not exist
+        ValueError
+            If the file could not be read
+        """
+
+        try:
+            return Table(pd.read_csv(path))
+        except FileNotFoundError as exception:
+            raise FileNotFoundError(f'File "{path}" does not exist') from exception
+        except Exception as exception:
+            raise ValueError(f'Could not read file from "{path}" as CSV') from exception
---
+++ Runtime/safe-ds/tests/data/test_table.py
@@ -0,0 +1,22 @@
+import pytest
+from safe_ds.data import Table
+
+
+def test_read_csv_valid():
+    table = Table.from_csv("tests/resources/test_table_read_csv.csv")
+    assert table._data["A"][0] == 1 and table._data["B"][0] == 2
+
+
+def test_read_csv_invalid():
+    with pytest.raises(FileNotFoundError):
+        Table.from_csv("tests/resources/test_table_read_csv_invalid.csv")
+
+
+def test_read_json_valid():
+    table = Table.from_json("tests/resources/test_table_read_json.json")
+    assert table._data["A"][0] == 1 and table._data["B"][0] == 2
+
+
+def test_read_json_invalid():
+    with pytest.raises(FileNotFoundError):
+        Table.from_json("tests/resources/test_table_read_json_invalid.json")
