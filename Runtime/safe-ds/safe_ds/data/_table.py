cbd28c25ea267c625b36957e70c68f246551c145
WinPlay02 <winplay02_gh@woberlaender.de>
2022-11-18 15:12:45 +0100

    feat: Access row of table by numerical index (#175)
    Closes #149
    
    ### Summary of Changes
    
    Added method to access row of table by numerical index + tests
    
    Co-authored-by: Andreas <s6asgerh@uni-bonn.de>
    Co-authored-by: Gerhardsa0 <andreasgerhards@t-online.de>
    Co-authored-by: WinPlay02 <WinPlay02@users.noreply.github.com>
    Co-authored-by: Lars Reimann <mail@larsreimann.com>

--- Runtime/safe-ds/safe_ds/data/_table.py
+++ Runtime/safe-ds/safe_ds/data/_table.py
@@ -1,12 +1,32 @@
 from __future__ import annotations
 
 import pandas as pd
+from safe_ds.data import Row
 from safe_ds.exceptions import ColumnNameDuplicateError, ColumnNameError
 
 
 class Table:
     def __init__(self, data: pd.DataFrame):
         self._data: pd.DataFrame = data
+
+    def get_row_by_index(self, index: int) -> Row:
+        """
+        returns the row of the Table for a given Index
+        Parameters
+        ----------
+        index : int
+
+        Returns
+        -------
+        a Row of the Table
+        Raises
+        ------
+        KeyError
+            if the index doesn't exist
+        """
+        if len(self._data.index) - 1 < index or index < 0:
+            raise KeyError
+        return Row(self._data.iloc[[index]].squeeze())
 
     @staticmethod
     def from_json(path: str) -> Table:
--- Runtime/safe-ds/tests/data/test_table.py
+++ Runtime/safe-ds/tests/data/test_table.py
@@ -23,6 +23,24 @@ def test_read_json_invalid():
         Table.from_json("tests/resources/test_table_read_json_invalid.json")
 
 
+def test_get_row_by_index():
+    table = Table.from_csv("tests/resources/test_table_read_csv.csv")
+    val = table.get_row_by_index(0)
+    assert val._data["A"] == 1 and val._data["B"] == 2
+
+
+def test_get_row_by_index_negative_index():
+    table = Table.from_csv("tests/resources/test_table_read_csv.csv")
+    with pytest.raises(KeyError):
+        table.get_row_by_index(-1)
+
+
+def test_get_row_by_index_out_of_bounds_index():
+    table = Table.from_csv("tests/resources/test_table_read_csv.csv")
+    with pytest.raises(KeyError):
+        table.get_row_by_index(5)
+
+
 @pytest.mark.parametrize(
     "name_from, name_to, column_one, column_two",
     [("A", "D", "D", "B"), ("A", "A", "A", "B")],
