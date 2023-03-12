25d633d4b481437361d24659965a1fd23aa8a9bf
Lars Reimann <mail@larsreimann.com>
2023-03-12 12:27:12 +0100

    ci: include configuration of semantic release in repo

---
+++ release.config.js
@@ -0,0 +1,36 @@
+// Based on https://github.com/bjoluc/semantic-release-config-poetry/blob/main/release.config.js
+
+const PYPI_REPOSITORY = 'https://upload.pypi.org/legacy/';
+
+module.exports = {
+    branches: ['main'],
+    plugins: [
+        ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
+        ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits' }],
+        ['@semantic-release/changelog', { changelogFile: 'docs/CHANGELOG.md' }],
+        [
+            '@semantic-release/exec',
+            {
+                verifyConditionsCmd: `if [ 403 != $(curl -X POST -F ':action=file_upload' -u __token__:$PYPI_TOKEN -s -o /dev/null -w '%{http_code}' ${PYPI_REPOSITORY}) ]; then (exit 0); else (echo 'Authentication error. Please check the PYPI_TOKEN environment variable.' && exit 1); fi`,
+                prepareCmd: 'poetry version ${nextRelease.version}',
+                publishCmd: 'poetry publish --build --username __token__ --password $PYPI_TOKEN --no-interaction -vvv',
+                execCwd: 'src',
+            },
+        ],
+        [
+            '@semantic-release/github',
+            {
+                assets: [
+                    { path: 'src/dist/*.tar.gz', label: 'sdist' },
+                    { path: 'src/dist/*.whl', label: 'wheel' },
+                ],
+            },
+        ],
+        [
+            '@semantic-release/git',
+            {
+                assets: ['src/pyproject.toml', 'docs/CHANGELOG.md'],
+            },
+        ],
+    ],
+};
