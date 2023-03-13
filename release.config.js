4adbf5edba1993a6c046484c5417552b03e9cd5b
Lars Reimann <mail@larsreimann.com>
2023-03-13 14:31:42 +0100

    chore: clean up (#34)
    ### Summary of Changes
    
    * Remove some outdated linter configs
    * Move linter config to `pyproject.toml` where possible
    * Move `pyproject.toml` and `poetry.lock` to top-level to be able to
    reuse `README.md` in `docs/`
    * Specify dependencies to build documentation using `poetry`

--- release.config.js
+++ release.config.js
@@ -14,22 +14,21 @@ module.exports = {
                 verifyConditionsCmd: `if [ 403 != $(curl -X POST -F ':action=file_upload' -u __token__:$PYPI_TOKEN -s -o /dev/null -w '%{http_code}' ${PYPI_REPOSITORY}) ]; then (exit 0); else (echo 'Authentication error. Please check the PYPI_TOKEN environment variable.' && exit 1); fi`,
                 prepareCmd: 'poetry version ${nextRelease.version}',
                 publishCmd: 'poetry publish --build --username __token__ --password $PYPI_TOKEN --no-interaction -vvv',
-                execCwd: 'src',
             },
         ],
         [
             '@semantic-release/github',
             {
                 assets: [
-                    { path: 'src/dist/*.tar.gz', label: 'sdist' },
-                    { path: 'src/dist/*.whl', label: 'wheel' },
+                    { path: 'dist/*.tar.gz', label: 'sdist' },
+                    { path: 'dist/*.whl', label: 'wheel' },
                 ],
             },
         ],
         [
             '@semantic-release/git',
             {
-                assets: ['src/pyproject.toml', 'docs/CHANGELOG.md'],
+                assets: ['pyproject.toml', 'docs/CHANGELOG.md'],
             },
         ],
     ],
