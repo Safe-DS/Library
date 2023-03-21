49fc816a9004e9c17a6852209b602598fb062610
Lars Reimann <mail@larsreimann.com>
2023-03-21 22:15:19 +0100

    test: failing tests for `count` and `len`

---
+++ tests/safeds/data/tabular/_row/test_count.py
@@ -0,0 +1,10 @@
+from safeds.data.tabular import Row
+from safeds.data.tabular.typing import StringColumnType, IntColumnType, TableSchema
+
+
+def test_count() -> None:
+    row = Row(
+        [0, "1"],
+        TableSchema({"testColumn1": IntColumnType(), "testColumn2": StringColumnType()})
+    )
+    assert row.count() == 2
