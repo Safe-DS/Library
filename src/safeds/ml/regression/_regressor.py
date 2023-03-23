c3701fb20d3539a7a2d828a7ba6d7dfb70ec85a3
Lars Reimann <mail@larsreimann.com>
2023-03-23 22:00:33 +0100

    refactor: add `fit` and `predict` abstract methods

--- src/safeds/ml/regression/_regressor.py
+++ src/safeds/ml/regression/_regressor.py
@@ -1,5 +1,48 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from safeds.data.tabular.containers import TaggedTable, Table
 
 
 class Regressor(ABC):
-    pass
+
+    @abstractmethod
+    def fit(self, tagged_table: TaggedTable) -> None:
+        """
+        Fit this model given a tagged table.
+
+        Parameters
+        ----------
+        tagged_table : TaggedTable
+            The tagged table containing the feature and target vectors.
+
+        Raises
+        ------
+        LearningError
+            If the tagged table contains invalid values or if the training failed.
+        """
+        pass
+
+    @abstractmethod
+    def predict(self, dataset: Table, target_name: Optional[str] = None) -> Table:
+        """
+        Predict a target vector using a dataset containing feature vectors. The model has to be trained first.
+
+        Parameters
+        ----------
+        dataset : Table
+            The dataset containing the feature vectors.
+        target_name: Optional[str]
+            The name of the target vector. The name of the target column inferred from fit is used by default.
+
+        Returns
+        -------
+        table : Table
+            A dataset containing the given feature vectors and the predicted target vector.
+
+        Raises
+        ------
+        PredictionError
+            If prediction with the given dataset failed.
+        """
+        pass
