0c7a8111bea66c690772471158829fa4945f85e0
Lars Reimann <mail@larsreimann.com>
2023-03-24 22:35:18 +0100

    feat: rename a `Column`

--- tests/safeds/data/tabular/containers/_column/test_rename.py
+++ tests/safeds/data/tabular/containers/_column/test_rename.py
@@ -1,13 +1,13 @@
 from safeds.data.tabular.containers import Column
 
 
-def test_should_return_new_column_with_new_name():
+def test_should_return_new_column_with_new_name() -> None:
     column = Column([1, 2, 3], "A")
     new_column = column.rename("B")
     assert new_column.name == "B"
 
 
-def test_should_not_change_name_of_original_column():
+def test_should_not_change_name_of_original_column() -> None:
     column = Column([1, 2, 3], "A")
     column.rename("B")
     assert column.name == "A"
