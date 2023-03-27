01319a962f973233e34dd9fab58dfd20bf7a9918
Lars Reimann <mail@larsreimann.com>
2023-03-27 22:56:09 +0200

    test: improve regressor tests

--- tests/safeds/ml/regression/test_decision_tree.py
+++ tests/safeds/ml/regression/test_decision_tree.py
@@ -1,5 +1,5 @@
 import pytest
-from safeds.data.tabular.containers import Table, TaggedTable
+from safeds.data.tabular.containers import Table, TaggedTable, Column
 from safeds.exceptions import LearningError, PredictionError
 from safeds.ml.regression import DecisionTree, Regressor
 from tests.fixtures import resolve_resource_path
@@ -37,12 +37,15 @@ class TestFit:
 
 
 class TestPredict:
-    def test_should_succeed_on_valid_data(
-        self, regressor: Regressor, valid_data: TaggedTable
-    ) -> None:
+    def test_should_include_features_of_prediction_input(self, regressor: Regressor, valid_data: TaggedTable) -> None:
         fitted_regressor = regressor.fit(valid_data)
-        fitted_regressor.predict(valid_data.features)
-        assert True  # This asserts that the predict method succeeds
+        prediction = fitted_regressor.predict(valid_data.features)
+        assert prediction.features == valid_data.features
+
+    def test_should_set_correct_target_name(self, regressor: Regressor, valid_data: TaggedTable) -> None:
+        fitted_regressor = regressor.fit(valid_data)
+        prediction = fitted_regressor.predict(valid_data.features)
+        assert prediction.target.name == "T"
 
     def test_should_raise_when_not_fitted(
         self, regressor: Regressor, valid_data: TaggedTable
