ddd3f59cf4f0173327511593ea4dc0b5b938fce1
Lars Reimann <mail@larsreimann.com>
2023-03-30 18:36:27 +0200

    feat: create empty `Table` without schema (#128)
    ### Summary of Changes
    
    We no longer raise an exception when a user creates an empty `Table`
    without specifying a schema. `Table([])` is now allows.
    
    `add_row` and `add_rows` must still be adjusted (#127) to allow adding
    rows to such an empty `Table`.
    
    ---------
    
    Co-authored-by: lars-reimann <lars-reimann@users.noreply.github.com>

--- src/safeds/data/tabular/containers/_table.py
+++ src/safeds/data/tabular/containers/_table.py
@@ -18,7 +18,6 @@ from safeds.exceptions import (
     DuplicateColumnNameError,
     IndexOutOfBoundsError,
     MissingDataError,
-    MissingSchemaError,
     NonNumericColumnError,
     SchemaMismatchError,
     UnknownColumnNameError,
@@ -193,14 +192,10 @@ class Table:
 
     def __init__(self, data: Iterable, schema: Optional[TableSchema] = None):
         self._data: pd.Dataframe = data if isinstance(data, pd.DataFrame) else pd.DataFrame(data)
-        if schema is None:
-            if self.count_columns() == 0:
-                raise MissingSchemaError()
-            self._schema: TableSchema = TableSchema._from_dataframe(self._data)
-        else:
-            self._schema = schema
-            if self._data.empty:
-                self._data = pd.DataFrame(columns=self._schema.get_column_names())
+        self._schema: TableSchema = TableSchema._from_dataframe(self._data) if schema is None else schema
+
+        if self._data.empty:
+            self._data = pd.DataFrame(columns=self._schema.get_column_names())
 
         self._data = self._data.reset_index(drop=True)
         self._data.columns = list(range(self.count_columns()))
--- tests/safeds/data/tabular/containers/_table/test_table.py
+++ tests/safeds/data/tabular/containers/_table/test_table.py
@@ -4,10 +4,13 @@ from safeds.data.tabular.typing import ColumnType, TableSchema
 
 
 def test_create_empty_table() -> None:
-    table = Table(
-        [], TableSchema({"col1": ColumnType.from_numpy_dtype(np.dtype(float))})
-    )
+    table = Table([], TableSchema({"col1": ColumnType.from_numpy_dtype(np.dtype(float))}))
     col = table.get_column("col1")
     assert col.count() == 0
     assert isinstance(col.type, type(ColumnType.from_numpy_dtype(np.dtype(float))))
     assert col.name == "col1"
+
+
+def test_create_empty_table_without_schema() -> None:
+    table = Table([])
+    assert table.schema == TableSchema({})
