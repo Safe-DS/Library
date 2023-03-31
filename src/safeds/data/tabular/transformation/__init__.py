2d778c6fa0541e796530d5e3fbbb7e972c6846e5
Lars Reimann <mail@larsreimann.com>
2023-03-31 20:38:31 +0200

    fix: export `TableTransformer` and `InvertibleTableTransformer`

--- src/safeds/data/tabular/transformation/__init__.py
+++ src/safeds/data/tabular/transformation/__init__.py
@@ -1,3 +1,4 @@
 from ._imputer import Imputer, ImputerStrategy
 from ._label_encoder import LabelEncoder
 from ._one_hot_encoder import OneHotEncoder
+from ._table_transformer import TableTransformer, InvertibleTableTransformer
