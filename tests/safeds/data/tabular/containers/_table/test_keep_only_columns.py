fc71c37166418979f9666192819cf63c9d5660d2
Lars Reimann <mail@larsreimann.com>
2023-04-16 22:03:49 +0200

    test: rename test function to match name of tested function

--- tests/safeds/data/tabular/containers/_table/test_keep_only_columns.py
+++ tests/safeds/data/tabular/containers/_table/test_keep_only_columns.py
@@ -5,7 +5,7 @@ from safeds.data.tabular.exceptions import UnknownColumnNameError
 from tests.helpers import resolve_resource_path
 
 
-def test_keep_columns() -> None:
+def test_keep_only_columns() -> None:
     table = Table.from_csv_file(resolve_resource_path("test_table_from_csv_file.csv"))
     transformed_table = table.keep_only_columns(["A"])
     assert transformed_table.schema.has_column("A")
