b8a81b1802aebcddf495e599b32b16346084fadf
Alexander Gréus <alexgreus51@gmail.com>
2023-04-18 00:54:33 +0200

    Added assertion to `test_should_return_original_table` to check the order of columns

--- tests/safeds/data/tabular/transformation/test_one_hot_encoder.py
+++ tests/safeds/data/tabular/transformation/test_one_hot_encoder.py
@@ -237,6 +237,7 @@ class TestInverseTransform:
         # This is subsumed by the next assertion, but we get a better error message
         assert result.schema == table_to_transform.schema
         assert result == table_to_transform
+        assert result.get_column_names() == table_to_transform.get_column_names()
 
     def test_should_not_change_transformed_table(self) -> None:
         table = Table.from_dict(
