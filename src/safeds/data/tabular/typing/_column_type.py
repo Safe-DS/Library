de2786529525d5b7a9823d88f9eb2afc2a95eeab
Lars Reimann <mail@larsreimann.com>
2023-04-18 21:20:42 +0200

    test: improve tests of column types

--- src/safeds/data/tabular/typing/_column_type.py
+++ src/safeds/data/tabular/typing/_column_type.py
@@ -4,11 +4,13 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
-from polars import FLOAT_DTYPES as POLARS_FLOAT_DTYPES
-from polars import INTEGER_DTYPES as POLARS_INTEGER_DTYPES
 from polars import Boolean as PolarsBoolean
 from polars import Decimal as PolarsDecimal
+from polars import FLOAT_DTYPES as POLARS_FLOAT_DTYPES
+from polars import INTEGER_DTYPES as POLARS_INTEGER_DTYPES
+from polars import Object as PolarsObject
 from polars import PolarsDataType
+from polars import TEMPORAL_DTYPES as POLARS_TEMPORAL_DTYPES
 from polars import Utf8 as PolarsUtf8
 
 if TYPE_CHECKING:
@@ -76,7 +78,7 @@ class ColumnType(ABC):
             return Boolean()
         if data_type in POLARS_FLOAT_DTYPES or data_type is PolarsDecimal:
             return RealNumber()
-        if data_type is PolarsUtf8:
+        if data_type is PolarsUtf8 or data_type is PolarsObject or data_type in POLARS_TEMPORAL_DTYPES:
             return String()
 
         message = f"Unsupported polars data type '{data_type}'."
