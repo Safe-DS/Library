2ff9d4ee09ada3a6c9aeb6d0e159e1956481f325
Lars Reimann <mail@larsreimann.com>
2023-04-21 20:17:20 +0200

    chore: undo some unintended changes

--- tests/safeds/ml/classical/test_util_sklearn.py
+++ tests/safeds/ml/classical/test_util_sklearn.py
@@ -1,16 +1,14 @@
-from __future__ import annotations
-
 import warnings
 
 from safeds.data.tabular.containers import Table
-from safeds.ml.classical.regression import SupportVectorMachine
+from safeds.ml.classical.regression import LinearRegression
 
 
 def test_predict_should_not_warn_about_feature_names() -> None:
-    """See https://github.com/Safe-DS/Stdlib/issues/154."""
+    """See https://github.com/Safe-DS/Stdlib/issues/51."""
     training_set = Table({"a": [1, 2, 3], "b": [2, 4, 6]}).tag_columns(target_name="b")
 
-    model = SupportVectorMachine()
+    model = LinearRegression()
     fitted_model = model.fit(training_set)
 
     test_set = Table({"a": [4, 5, 6]})
