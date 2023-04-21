3cf46c9342d454265c1b19703f783eccbcdf3a29
alex-senger <s6alseng@uni-bonn.de>
2023-04-21 13:55:06 +0200

    fix: add match parameter for pytest.raises() and check if classifier is not None

--- tests/safeds/ml/classical/regression/test_random_forest.py
+++ tests/safeds/ml/classical/regression/test_random_forest.py
@@ -5,7 +5,7 @@ from safeds.ml.classical.regression import RandomForest
 
 
 def test_number_of_trees_invalid() -> None:
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="The number of trees has to be greater than 0."):
         RandomForest(-1)
 
 
@@ -14,4 +14,5 @@ def test_number_of_trees_valid() -> None:
     tagged_training_set = training_set.tag_columns(target_name="col1", feature_names=["col2"])
 
     random_forest = RandomForest(10).fit(tagged_training_set)
-    assert (random_forest._wrapped_regressor.n_estimators == 10)
+    assert random_forest._wrapped_regressor is not None
+    assert random_forest._wrapped_regressor.n_estimators == 10
