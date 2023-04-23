d42903af3f185d49748f27759abfb341b733caae
Lars Reimann <mail@larsreimann.com>
2023-04-23 15:21:56 +0200

    test: add more tests for columns

---
+++ tests/safeds/data/tabular/containers/_column/test_none.py
@@ -0,0 +1,24 @@
+import pytest
+
+from safeds.data.tabular.containers import Column
+
+
+@pytest.mark.parametrize(
+    ("column", "predicate", "expected"),
+    [
+        (Column("a", []), lambda value: value == 1, True),
+        (Column("a", [1]), lambda value: value == 1, False),
+        (Column("a", [1, 2]), lambda value: value == 1, False),
+        (Column("a", [2, 2]), lambda value: value == 1, True),
+    ],
+    ids=[
+        "empty",
+        "predicate always true",
+        "predicate sometimes true",
+        "predicate always false",
+    ],
+)
+def test_should_return_true_if_no_values_satisfy_the_predicate(
+    column: Column, predicate: callable, expected: bool
+) -> None:
+    assert column.none(predicate) == expected
