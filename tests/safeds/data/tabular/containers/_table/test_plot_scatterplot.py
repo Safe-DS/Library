0dcf2bf09d58452f53b757ace72a57919169421a
PhilipGutberlet <92990487+PhilipGutberlet@users.noreply.github.com>
2023-04-28 11:08:30 +0200

    fixed positional argument error in test_plot_scatterplot_legacy_check()

--- tests/safeds/data/tabular/containers/_table/test_plot_scatterplot.py
+++ tests/safeds/data/tabular/containers/_table/test_plot_scatterplot.py
@@ -22,6 +22,6 @@ def test_plot_scatterplot_wrong_column_name() -> None:
 def test_plot_scatterplot_legacy_check() -> None:
     table = Table.from_dict({"A": [1, 2, 3], "B": [2, 4, 7]})
     #table.plot_scatterplot("A", "B").to_png_file(resolve_resource_path("./image/legacy_scatterplot.png"))
-    current = table.plot_scatterplot()
+    current = table.plot_scatterplot("A", "B")
     legacy = Image.from_png_file(resolve_resource_path("./image/legacy_scatterplot.png"))
     assert legacy._image.tobytes() == current._image.tobytes()
