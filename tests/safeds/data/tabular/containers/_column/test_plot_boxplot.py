a610617b5101e6e5a27dc71cc6843095c602151c
Philip Gutberlet <92990487+PhilipGutberlet@users.noreply.github.com>
2023-04-28 13:01:33 +0200

    Update tests/safeds/data/tabular/containers/_column/test_plot_boxplot.py
    Co-authored-by: Lars Reimann <mail@larsreimann.com>

--- tests/safeds/data/tabular/containers/_column/test_plot_boxplot.py
+++ tests/safeds/data/tabular/containers/_column/test_plot_boxplot.py
@@ -37,5 +37,5 @@ def test_plot_boxplot_legacy() -> None:
     # table.get_column("A").plot_boxplot().to_png_file(resolve_resource_path("./image/snapshot_boxplot.png"))
     table.get_column("A").plot_boxplot()
     current = table.get_column("A").plot_boxplot()
-    legacy = Image.from_png_file(resolve_resource_path("./image/snapshot_boxplot.png"))
+    snapshot = Image.from_png_file(resolve_resource_path("./image/snapshot_boxplot.png"))
     assert legacy._image.tobytes() == current._image.tobytes()
