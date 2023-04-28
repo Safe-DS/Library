48dc656b27cda2549f1137303208f14db593766a
daniaHu <s5dahuss@uni-bonn.de>
2023-04-28 16:15:24 +0200

    test_shuffle_rows.py modified

--- tests/safeds/data/tabular/containers/_table/test_shuffle_rows.py
+++ tests/safeds/data/tabular/containers/_table/test_shuffle_rows.py
@@ -1,7 +1,13 @@
 from safeds.data.tabular.containers import Table
+import pytest
 
-
-def test_shuffle_rows_valid() -> None:
-    table = Table.from_dict({"col1": [1], "col2": [1]})
+@pytest.mark.parametrize(
+    ("table"),
+    [
+        (Table.from_dict({"col1": [1], "col2": [1]})),
+    ],
+    ids=["Table with identical values in rows"],
+)
+def test_should_shuffle_rows(table: Table) -> None:
     result_table = table.shuffle_rows()
     assert table == result_table
