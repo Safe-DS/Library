9145d920c98b17bfd3171f8748c7baf47f46c1df
Alexander Gréus <alexgreus51@gmail.com>
2023-05-05 15:54:31 +0200

    refactor: Improved error messages for `GradientBoosting` and `SupportVectorMachine`
    Co-authored-by: alex-senger <91055000+alex-senger@users.noreply.github.com>

--- tests/safeds/ml/classical/classification/test_gradient_boosting.py
+++ tests/safeds/ml/classical/classification/test_gradient_boosting.py
@@ -9,6 +9,21 @@ def training_set() -> TaggedTable:
     return table.tag_columns(target_name="col1", feature_names=["col2"])
 
 
+class TestNumberOfTrees:
+    def test_should_be_passed_to_fitted_model(self, training_set: TaggedTable) -> None:
+        fitted_model = GradientBoosting(number_of_trees=2).fit(training_set)
+        assert fitted_model._number_of_trees == 2
+
+    def test_should_be_passed_to_sklearn(self, training_set: TaggedTable) -> None:
+        fitted_model = GradientBoosting(number_of_trees=2).fit(training_set)
+        assert fitted_model._wrapped_classifier is not None
+        assert fitted_model._wrapped_classifier.n_estimators == 2
+
+    def test_should_raise_if_less_than_1(self) -> None:
+        with pytest.raises(ValueError, match="The parameter 'number_of_trees' has to be greater than 0."):
+            GradientBoosting(number_of_trees=0)
+
+
 class TestLearningRate:
     def test_should_be_passed_to_fitted_model(self, training_set: TaggedTable) -> None:
         fitted_model = GradientBoosting(learning_rate=2).fit(training_set)
@@ -22,18 +37,3 @@ class TestLearningRate:
     def test_should_raise_if_less_than_or_equal_to_0(self) -> None:
         with pytest.raises(ValueError, match="The parameter 'learning_rate' has to be greater than 0."):
             GradientBoosting(learning_rate=-1)
-
-
-class TestNumberOfTrees:
-    def test_should_be_passed_to_fitted_model(self, training_set: TaggedTable) -> None:
-        fitted_model = GradientBoosting(number_of_trees=2).fit(training_set)
-        assert fitted_model._number_of_trees == 2
-
-    def test_should_be_passed_to_sklearn(self, training_set: TaggedTable) -> None:
-        fitted_model = GradientBoosting(number_of_trees=2).fit(training_set)
-        assert fitted_model._wrapped_classifier is not None
-        assert fitted_model._wrapped_classifier.n_estimators == 2
-
-    def test_should_raise_if_less_than_1(self) -> None:
-        with pytest.raises(ValueError, match="The number of boosting stages to perform has to be greater than 0."):
-            GradientBoosting(number_of_trees=0)
