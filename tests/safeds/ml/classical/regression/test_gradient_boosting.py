766f2ff2a6d68098be3e858ad12bf9e509e5f192
Alexander <47296670+Marsmaennchen221@users.noreply.github.com>
2023-05-05 14:43:31 +0200

    feat: Added parameter `number_of_trees` to `GradientBoosting` (#268)
    Closes #170.
    
    ### Summary of Changes
    
    Added parameter `number_of_trees` to `GradientBoosting`
    
    ---------
    
    Co-authored-by: alex-senger <91055000+alex-senger@users.noreply.github.com>
    Co-authored-by: megalinter-bot <129584137+megalinter-bot@users.noreply.github.com>
    Co-authored-by: Lars Reimann <mail@larsreimann.com>

--- tests/safeds/ml/classical/regression/test_gradient_boosting.py
+++ tests/safeds/ml/classical/regression/test_gradient_boosting.py
@@ -20,5 +20,20 @@ class TestLearningRate:
         assert fitted_model._wrapped_regressor.learning_rate == 2
 
     def test_should_raise_if_less_than_or_equal_to_0(self) -> None:
-        with pytest.raises(ValueError, match="has to be greater than 0"):
+        with pytest.raises(ValueError, match="The learning rate has to be greater than 0."):
             GradientBoosting(learning_rate=-1)
+
+
+class TestNumberOfTrees:
+    def test_should_be_passed_to_fitted_model(self, training_set: TaggedTable) -> None:
+        fitted_model = GradientBoosting(number_of_trees=2).fit(training_set)
+        assert fitted_model._number_of_trees == 2
+
+    def test_should_be_passed_to_sklearn(self, training_set: TaggedTable) -> None:
+        fitted_model = GradientBoosting(number_of_trees=2).fit(training_set)
+        assert fitted_model._wrapped_regressor is not None
+        assert fitted_model._wrapped_regressor.n_estimators == 2
+
+    def test_should_raise_if_less_than_1(self) -> None:
+        with pytest.raises(ValueError, match="The number of boosting stages to perform has to be greater than 0."):
+            GradientBoosting(number_of_trees=0)
