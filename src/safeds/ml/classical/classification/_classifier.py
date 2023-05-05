70975e0b46a7aa09e9a4fefabc3644b9f2192093
PhilipGutberlet <92990487+PhilipGutberlet@users.noreply.github.com>
2023-05-05 14:42:23 +0200

    added recall function to classifier

--- src/safeds/ml/classical/classification/_classifier.py
+++ src/safeds/ml/classical/classification/_classifier.py
@@ -136,3 +136,40 @@ class Classifier(ABC):
         if (n_true_positives + n_false_positives) == 0:
             return 1.0
         return n_true_positives / (n_true_positives + n_false_positives)
+
+    def recall(self, validation_or_test_set: TaggedTable, positive_class: int = 1) -> float:
+        """
+        Compute the classifier's recall on the given data.
+
+        Parameters
+        ----------
+        validation_or_test_set : TaggedTable
+            The validation or test set.
+        positive_class : int | str
+            The class to be considered positive. All other classes are considered negative.
+
+        Returns
+        -------
+        recall : float
+            The calculated recall score, i.e. the ratio of correctly predicted positives to all expected positives.
+            Return 1 if there are no positive expectations.
+        """
+        if not isinstance(validation_or_test_set, TaggedTable) and isinstance(validation_or_test_set, Table):
+            raise UntaggedTableError
+
+        expected_values = validation_or_test_set.target
+        predicted_values = self.predict(validation_or_test_set.features).target
+
+        n_true_positives = 0
+        n_false_negatives = 0
+
+        for expected_value, predicted_value in zip(expected_values, predicted_values, strict=True):
+            if predicted_value == positive_class:
+                if expected_value == positive_class:
+                    n_true_positives += 1
+            elif expected_value == positive_class:
+                n_false_negatives += 1
+
+        if (n_true_positives + n_false_negatives) == 0:
+            return 1.0
+        return n_true_positives / (n_true_positives + n_false_negatives)
