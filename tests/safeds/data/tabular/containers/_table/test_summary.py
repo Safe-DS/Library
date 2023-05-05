619c40668558be4661b0c6096f67661703615deb
daniaHu <s5dahuss@uni-bonn.de>
2023-05-05 10:01:48 +0200

    test_summary.py modified

--- tests/safeds/data/tabular/containers/_table/test_summary.py
+++ tests/safeds/data/tabular/containers/_table/test_summary.py
@@ -1,47 +1,53 @@
+import pytest
 from safeds.data.tabular.containers import Table
+from statistics import stdev
 
 
-def test_summary() -> None:
-    table = Table.from_dict({"col1": [1, 2, 1], "col2": ["a", "b", "c"]})
-
-    truth = Table.from_dict(
-        {
-            "metrics": [
-                "maximum",
-                "minimum",
-                "mean",
-                "mode",
-                "median",
-                "sum",
-                "variance",
-                "standard deviation",
-                "idness",
-                "stability",
-            ],
-            "col1": [
-                "2",
-                "1",
-                str(4.0 / 3),
-                "[1]",
-                "1.0",
-                "4",
-                str(1.0 / 3),
-                str(table._data["col1"].std()),
-                str(2.0 / 3),
-                str(2.0 / 3),
-            ],
-            "col2": [
-                "-",
-                "-",
-                "-",
-                "['a', 'b', 'c']",
-                "-",
-                "-",
-                "-",
-                "-",
-                "1.0",
-                str(1.0 / 3),
-            ],
-        },
-    )
+@pytest.mark.parametrize(
+    ("table", "truth"),
+    [
+        (Table.from_dict({"col1": [1, 2, 1], "col2": ["a", "b", "c"]}),
+         Table.from_dict({
+             "metrics": [
+                 "maximum",
+                 "minimum",
+                 "mean",
+                 "mode",
+                 "median",
+                 "sum",
+                 "variance",
+                 "standard deviation",
+                 "idness",
+                 "stability",
+             ],
+             "col1": [
+                 "2",
+                 "1",
+                 str(4.0 / 3),
+                 "[1]",
+                 "1.0",
+                 "4",
+                 str(1.0 / 3),
+                 str(stdev([1, 2, 1])),
+                 str(2.0 / 3),
+                 str(2.0 / 3),
+             ],
+             "col2": [
+                 "-",
+                 "-",
+                 "-",
+                 "['a', 'b', 'c']",
+                 "-",
+                 "-",
+                 "-",
+                 "-",
+                 "1.0",
+                 str(1.0 / 3),
+             ],
+         },
+         )
+         )
+    ]
+)
+def test_summary(table, truth) -> None:
     assert truth == table.summary()
