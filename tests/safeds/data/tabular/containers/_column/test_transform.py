726cbeaa780eaa655758928ebbe9e69f1314521c
Lars Reimann <mail@larsreimann.com>
2023-05-06 10:04:04 +0200

    test: original column is not changed

--- tests/safeds/data/tabular/containers/_column/test_transform.py
+++ tests/safeds/data/tabular/containers/_column/test_transform.py
@@ -13,3 +13,16 @@ from safeds.data.tabular.containers import Column
 )
 def test_should_transform_column(column: Column, expected: Column) -> None:
     assert column.transform(lambda it: it + 1) == expected
+
+@pytest.mark.parametrize(
+    ("column", "original"),
+    [
+        (Column("test", []), Column("test", [])),
+        (Column("test", [1, 2]), Column("test", [1, 2])),
+        (Column("test", [-0.5, 0, 4]), Column("test", [-0.5, 0, 4])),
+    ],
+    ids=["empty", "integers", "floats"],
+)
+def test_should_not_change_original_column(column: Column, original: Column) -> None:
+    column.transform(lambda it: it + 1)
+    assert column == original
