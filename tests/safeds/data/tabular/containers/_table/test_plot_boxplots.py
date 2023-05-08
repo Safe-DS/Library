5eb6908b500505fbfc8ecd9a0a4e6139c8c8c31c
Alexander Gréus <alexgreus51@gmail.com>
2023-05-08 20:59:53 +0200

    refactor: Changed import of `NonNumericColumnError` after the merge

--- tests/safeds/data/tabular/containers/_table/test_plot_boxplots.py
+++ tests/safeds/data/tabular/containers/_table/test_plot_boxplots.py
@@ -2,7 +2,7 @@ import pytest
 
 from safeds.data.image.containers import Image
 from safeds.data.tabular.containers import Table
-from safeds.data.tabular.exceptions import NonNumericColumnError
+from safeds.exceptions import NonNumericColumnError
 
 from tests.helpers import resolve_resource_path
 
