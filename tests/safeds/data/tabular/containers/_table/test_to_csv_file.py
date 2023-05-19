c11299f8168d358b92a227a8ec360593a95a8bf7
patrikguempel <patrikguempel@gmail.com>
2023-05-19 15:10:21 +0200

    undone path test remove

--- tests/safeds/data/tabular/containers/_table/test_to_csv_file.py
+++ tests/safeds/data/tabular/containers/_table/test_to_csv_file.py
@@ -23,3 +23,21 @@ def test_should_create_csv_file_from_table_by_str(table: Table) -> None:
         with Path(tmp_table_file.name).open("r", encoding="utf-8") as tmp_file:
             table_r = Table.from_csv_file(tmp_file.name)
     assert table_r == table
+
+
+@pytest.mark.parametrize(
+    "table",
+    [
+        (Table({"col1": ["col1_1"], "col2": ["col2_1"]})),
+        (Table()),
+    ],
+    ids=["by String", "empty"]
+)
+def test_should_create_csv_file_from_table_by_path(table: Table) -> None:
+    with NamedTemporaryFile(suffix=".xlsx") as tmp_table_file:
+        tmp_table_file.close()
+        with Path(tmp_table_file.name).open("w", encoding="utf-8") as tmp_file:
+            table.to_csv_file(Path(tmp_file.name))
+        with Path(tmp_table_file.name).open("r", encoding="utf-8") as tmp_file:
+            table_r = Table.from_csv_file(Path(tmp_file.name))
+    assert table == table_r
