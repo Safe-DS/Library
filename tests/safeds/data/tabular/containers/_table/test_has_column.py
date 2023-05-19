8be21a355f29952d69cd590df31dd985fa9a05ba
patrikguempel <patrikguempel@gmail.com>
2023-05-19 11:16:38 +0200

    added test for empty table in test_has_column.py

--- tests/safeds/data/tabular/containers/_table/test_has_column.py
+++ tests/safeds/data/tabular/containers/_table/test_has_column.py
@@ -4,8 +4,12 @@ from safeds.data.tabular.containers import Table
 
 @pytest.mark.parametrize(
     ("table", "column", "expected"),
-    [(Table({"A": [1], "B": [2]}), "A", True), (Table({"A": [1], "B": [2]}), "C", False)],
-    ids=["has column", "doesn't have column"],
+    [
+        (Table({"A": [1], "B": [2]}), "A", True),
+        (Table({"A": [1], "B": [2]}), "C", False),
+        (Table(), "C", False)
+    ],
+    ids=["has column", "doesn't have column", "empty"],
 )
 def test_should_return_if_column_is_in_table(table: Table, column: str, expected: bool) -> None:
     assert table.has_column(column) == expected
