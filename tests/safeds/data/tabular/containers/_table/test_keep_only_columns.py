ab5dd0f2f982d1b24da9694ef768aaf997b99563
patrikguempel <patrikguempel@gmail.com>
2023-05-19 11:31:23 +0200

    created tests for empty table within test_inverse_transform_table.py, test_keep_only_columns.py, test_number_of_columns.py, test_number_of_rows.py

--- tests/safeds/data/tabular/containers/_table/test_keep_only_columns.py
+++ tests/safeds/data/tabular/containers/_table/test_keep_only_columns.py
@@ -32,15 +32,26 @@ from safeds.exceptions import UnknownColumnNameError
             ["C", "A"],
             Table({"C": [3], "A": [1]}),
         ),
+        (
+            Table(),
+            [],
+            Table(),
+        ),
     ],
-    ids=["No Column Name", "First Column", "Second Column", "All columns", "Last and first columns"],
+    ids=["No Column Name", "First Column", "Second Column", "All columns", "Last and first columns", "empty"],
 )
 def test_should_keep_only_listed_columns(table: Table, column_names: list[str], expected: Table) -> None:
     transformed_table = table.keep_only_columns(column_names)
     assert transformed_table == expected
 
-
-def test_should_raise_error_if_column_name_unknown() -> None:
-    table = Table({"A": [1], "B": [2]})
+@pytest.mark.parametrize(
+    "table",
+    [
+        Table({"A": [1], "B": [2]}),
+        Table()
+    ],
+    ids=["table", "empty"]
+)
+def test_should_raise_error_if_column_name_unknown(table) -> None:
     with pytest.raises(UnknownColumnNameError):
         table.keep_only_columns(["C"])
