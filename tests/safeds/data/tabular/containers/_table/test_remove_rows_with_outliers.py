4b4322b15d3a9fc9ea92060e2803c32a1869e21d
Alexander Gréus <alexgreus51@gmail.com>
2023-05-19 14:14:42 +0200

    test: Added test for `Table.remove_rows_with_outliers` to test multiple outliers in one column and outliers in two different columns

--- tests/safeds/data/tabular/containers/_table/test_remove_rows_with_outliers.py
+++ tests/safeds/data/tabular/containers/_table/test_remove_rows_with_outliers.py
@@ -13,7 +13,13 @@ from safeds.data.tabular.containers import Table
                     "col3": [2, 3, 1],
                 },
             ),
-            3,
+            Table(
+                {
+                    "col1": ["A", "B", "C"],
+                    "col2": [1.0, 2.0, 3.0],
+                    "col3": [2, 3, 1],
+                },
+            ),
         ),
         (
             Table(
@@ -36,7 +42,127 @@ from safeds.data.tabular.containers import Table
                     "col3": [2, 3, 1, 1_000_000_000, 1, 1, 1, 1, 1, 1, 1, 1],
                 },
             ),
-            11,
+            Table(
+                {
+                    "col1": [
+                        "A",
+                        "B",
+                        "A",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                    ],
+                    "col2": [1.0, 2.0, 3.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, None],
+                    "col3": [2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                },
+            ),
+        ),
+        (
+            Table(
+                {
+                    "col1": [
+                        "A",
+                        "B",
+                        "A",
+                        "outlier_col3",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "outlier_col2",
+                        "a",
+                    ],
+                    "col2": [1.0, 2.0, 3.0, 4.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1000.0, None],
+                    "col3": [2, 3, 1, 1_000_000_000, 1, 1, 1, 1, 1, 1, 1, 1],
+                },
+            ),
+            Table(
+                {
+                    "col1": [
+                        "A",
+                        "B",
+                        "A",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                    ],
+                    "col2": [1.0, 2.0, 3.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, None],
+                    "col3": [2, 3, 1, 1, 1, 1, 1, 1, 1, 1],
+                },
+            ),
+        ),
+        (
+            Table(
+                {
+                    "col1": [
+                        "A",
+                        "B",
+                        "A",
+                        "positive_outlier",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "negative_outlier",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                    ],
+                    "col2": [1.0, 2.0, 3.0, 4.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, None, 1.0, 2.0, 1.0, 4.0, 1.0, 3.0, 1.0, 2.0, 1.0, 4.0, 1.0],
+                    "col3": [2, 3, 1, 1_000_000_000_000, 1, 1, 1, 1, 1, 1, 1, 1, -1_000_000_000_000, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                },
+            ),
+            Table(
+                {
+                    "col1": [
+                        "A",
+                        "B",
+                        "A",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                        "a",
+                    ],
+                    "col2": [1.0, 2.0, 3.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, None, 2.0, 1.0, 4.0, 1.0, 3.0, 1.0, 2.0, 1.0, 4.0, 1.0],
+                    "col3": [2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                },
+            ),
         ),
         (
             Table(
@@ -45,11 +171,17 @@ from safeds.data.tabular.containers import Table
                     "col2": [],
                 },
             ),
-            0,
+            Table(
+                {
+                    "col1": [],
+                    "col2": [],
+                },
+            ),
         ),
     ],
-    ids=["no outliers", "with outliers", "no rows"],
+    ids=["no outliers", "one outlier", "outliers in two different columns", "multiple outliers in one column", "no rows"],
 )
-def test_should_remove_rows_with_no_outliers(table: Table, expected: int) -> None:
+def test_should_remove_rows_with_no_outliers(table: Table, expected: Table) -> None:
     updated_table = table.remove_rows_with_outliers()
-    assert updated_table.number_of_rows == expected
+    assert updated_table.schema == expected.schema
+    assert updated_table == expected
