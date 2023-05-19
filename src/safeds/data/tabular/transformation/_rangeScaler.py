9cc0e353d4cef1806d1c2b42a40180e61c19403a
Simon <s6snbreu@uni-bonn.de>
2023-05-19 14:01:12 +0200

    added RangeScaler class

---
+++ src/safeds/data/tabular/transformation/_rangeScaler.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import Any
+
+import pandas as pd
+
+from sklearn import MinMaxScaler
+from safeds.data.tabular.containers import Table
+from safeds.data.tabular.transformation._table_transformer import TableTransformer
+
+
+class RangeScaler(TableTransformer):
+    """
+    Normalize Values in a Table
+
+    Parameters
+    ----------
+
+    Examples
+    --------
+    """
+    pass
