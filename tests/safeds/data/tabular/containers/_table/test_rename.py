e394902e75a99599c0d651d55e8fb0b0a5e6fdcd
daniaHu <s5dahuss@uni-bonn.de>
2023-05-23 20:25:02 +0200

    reduced repetition in implementation of test_should_rename_column in test_rename.py

--- tests/safeds/data/tabular/containers/_table/test_rename.py
+++ tests/safeds/data/tabular/containers/_table/test_rename.py
@@ -1,19 +1,21 @@
+import pandas as pd
 import pytest
 from safeds.data.tabular.containers import Table
+from safeds.data.tabular.typing import Integer
 from safeds.exceptions import DuplicateColumnNameError, UnknownColumnNameError
 
 
 @pytest.mark.parametrize(
     ("name_from", "name_to", "column_one", "column_two"),
-    [("A", "D", "D", "B"), ("A", "A", "A", "B")],
+    [("A", "D", "D", "B"),
+     ("A", "A", "A", "B")],
     ids=["column renamed", "column not renamed"],
 )
 def test_should_rename_column(name_from: str, name_to: str, column_one: str, column_two: str) -> None:
     table: Table = Table({"A": [1], "B": [2]})
     renamed_table = table.rename_column(name_from, name_to)
-    assert renamed_table.schema.has_column(column_one)
-    assert renamed_table.schema.has_column(column_two)
-    assert renamed_table.number_of_columns == 2
+    assert renamed_table.schema.column_names == [column_one, column_two]
+    assert renamed_table.column_names == [column_one, column_two]
 
 
 def test_should_raise_if_old_column_does_not_exist() -> None:
