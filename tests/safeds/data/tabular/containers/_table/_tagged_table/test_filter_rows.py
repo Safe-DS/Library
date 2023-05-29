e4f9b19b4735d64ac141ae4afb046d7804377d0d
Severin Paul Höfer <84280965+zzril@users.noreply.github.com>
2023-05-29 20:55:29 +0200

    Add test for filter_rows

---
+++ tests/safeds/data/tabular/containers/_table/_tagged_table/test_filter_rows.py
@@ -0,0 +1,27 @@
+from safeds.data.tabular.containers import TaggedTable
+
+
+def test_should_remove_row() -> None:
+    table = TaggedTable(
+        {
+            "feature_1": [3, 9, 6],
+            "feature_2": [6, 12, 9],
+            "target": [1, 3, 2],
+        },
+        "target",
+        None
+    )
+    new_table = table.filter_rows(lambda row: all(row.get_value(col) < 10 for col in table.column_names))
+    expected = TaggedTable(
+        {
+            "feature_1": [3, 6],
+            "feature_2": [6, 9],
+            "target": [1, 2],
+        },
+        "target",
+        None
+    )
+    assert new_table.schema == expected.schema
+    assert new_table.features == expected.features
+    assert new_table.target == expected.target
+    assert new_table == expected
