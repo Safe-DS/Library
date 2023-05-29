1183c3ee8488f2a87201a687d66a985d335c6711
Severin Paul Höfer <84280965+zzril@users.noreply.github.com>
2023-05-29 13:43:47 +0200

    Add test for overriden add_columns method

---
+++ tests/safeds/data/tabular/containers/_table/_tagged_table/test_add_column.py
@@ -0,0 +1,27 @@
+import pytest
+
+from safeds.data.tabular.containers import TaggedTable, Column
+
+
+def test_should_add_column():
+    table = TaggedTable(
+        {
+            "feature_1": [0, 1, 2],
+            "target": [3, 4, 5],
+        },
+        "target",
+        None,
+    )
+    col = Column("feature_2", [6, 7, 8])
+    new_table = table.add_column(col)
+    expected = TaggedTable(
+        {
+            "feature_1": [0, 1, 2],
+            "target": [3, 4, 5],
+            "feature_2": [6, 7, 8],
+        },
+        "target",
+        None,
+    )
+    assert new_table.schema == expected.schema
+    assert new_table == expected
