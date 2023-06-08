ddaa8508608dbe12a274230672958bcf91e80ace
jxnior01 <atemebangashu@gmail.com>
2023-06-08 07:19:36 +0200

    edited tests files to test the kernel parameter in the SupportVectorMachine for classification and Regression.

--- tests/safeds/ml/classical/regression/test_support_vector_machine.py
+++ tests/safeds/ml/classical/regression/test_support_vector_machine.py
@@ -1,6 +1,6 @@
 import pytest
 from safeds.data.tabular.containers import Table, TaggedTable
-from safeds.ml.classical.regression import SupportVectorMachine
+from safeds.ml.classical.regression._support_vector_machine import SupportVectorMachine
 
 
 @pytest.fixture()
@@ -11,13 +11,17 @@ def training_set() -> TaggedTable:
 
 class TestC:
     def test_should_be_passed_to_fitted_model(self, training_set: TaggedTable) -> None:
-        fitted_model = SupportVectorMachine(c=2).fit(training_set=training_set)
+        kernel = SupportVectorMachine.Kernel.Linear()
+        fitted_model = SupportVectorMachine(c=2, kernel=kernel).fit(training_set=training_set)
         assert fitted_model._c == 2
+        assert isinstance(fitted_model.kernel, SupportVectorMachine.Kernel.Linear)
 
     def test_should_be_passed_to_sklearn(self, training_set: TaggedTable) -> None:
-        fitted_model = SupportVectorMachine(c=2).fit(training_set)
+        kernel = SupportVectorMachine.Kernel.Linear()
+        fitted_model = SupportVectorMachine(c=2, kernel=kernel).fit(training_set)
         assert fitted_model._wrapped_regressor is not None
         assert fitted_model._wrapped_regressor.C == 2
+        assert isinstance(fitted_model.kernel, SupportVectorMachine.Kernel.Linear)
 
     def test_should_raise_if_less_than_or_equal_to_0(self) -> None:
         with pytest.raises(
