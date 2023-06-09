1e3e8fc7c69d65baa16afc554f3264ca4224bd2e
methr0 <s6tameth@uni-bonn.de>
2023-06-09 16:01:00 +0200

    add test for png image

--- tests/safeds/data/image/containers/test_image.py
+++ tests/safeds/data/image/containers/test_image.py
@@ -164,8 +164,14 @@ class TestResize:
                 3,
                 (2, 3),
             ),
+            (
+                Image.from_png_file(resolve_resource_path("image/white_square.png")),
+                2,
+                3,
+                (2, 3),
+            ),
         ],
-        ids=["(2, 3)"]
+        ids=[".jpg", ".png"]
     )
     def test_should_return_resized_image(self, image: Image, new_width: int, new_height: int, new_size: tuple[int, int]) -> None:
         assert image.resize(new_width, new_height)._image.size == new_size
