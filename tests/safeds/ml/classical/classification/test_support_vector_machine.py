e765dd80ee0925fa36e0cfd103cd50fc3600db94
jxnior01 <atemebangashu@gmail.com>
2023-06-09 10:39:39 +0200

    tests classification: added test_should_get_sklearn_kernel_linear for code coverage

--- tests/safeds/ml/classical/classification/test_support_vector_machine.py
+++ tests/safeds/ml/classical/classification/test_support_vector_machine.py
@@ -23,6 +23,11 @@ class TestC:
         assert fitted_model._wrapped_classifier.C == 2
         assert isinstance(fitted_model.kernel, SupportVectorMachine.Kernel.Linear)
 
+    def test_should_get_sklearn_kernel_linear(self) -> None:
+        svm = SupportVectorMachine(c=2, kernel=SupportVectorMachine.Kernel.Linear())
+        linear_kernel = svm.kernel.get_sklearn_kernel()
+        assert linear_kernel == "linear"
+
     def test_should_raise_if_less_than_or_equal_to_0(self) -> None:
         with pytest.raises(
             ValueError,
