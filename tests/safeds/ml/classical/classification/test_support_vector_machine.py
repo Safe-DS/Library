30caa1aabc4ecf113a718564bcb7e553b5c0964a
jxnior01 <atemebangashu@gmail.com>
2023-06-09 15:59:54 +0200

    changes as per review

--- tests/safeds/ml/classical/classification/test_support_vector_machine.py
+++ tests/safeds/ml/classical/classification/test_support_vector_machine.py
@@ -11,17 +11,13 @@ def training_set() -> TaggedTable:
 
 class TestC:
     def test_should_be_passed_to_fitted_model(self, training_set: TaggedTable) -> None:
-        kernel = SupportVectorMachine.Kernel.Linear()
-        fitted_model = SupportVectorMachine(c=2, kernel=kernel).fit(training_set=training_set)
+        fitted_model = SupportVectorMachine(c=2).fit(training_set=training_set)
         assert fitted_model.c == 2
-        assert isinstance(fitted_model.kernel, SupportVectorMachine.Kernel.Linear)
 
     def test_should_be_passed_to_sklearn(self, training_set: TaggedTable) -> None:
-        kernel = SupportVectorMachine.Kernel.Linear()
-        fitted_model = SupportVectorMachine(c=2, kernel=kernel).fit(training_set)
+        fitted_model = SupportVectorMachine(c=2).fit(training_set)
         assert fitted_model._wrapped_classifier is not None
         assert fitted_model._wrapped_classifier.C == 2
-        assert isinstance(fitted_model.kernel, SupportVectorMachine.Kernel.Linear)
 
     def test_should_raise_if_less_than_or_equal_to_0(self) -> None:
         with pytest.raises(
@@ -32,6 +28,17 @@ class TestC:
 
 
 class TestKernel:
+    def test_should_be_passed_to_fitted_model(self, training_set: TaggedTable) -> None:
+        kernel = SupportVectorMachine.Kernel.Linear()
+        fitted_model = SupportVectorMachine(c=2, kernel=kernel).fit(training_set=training_set)
+        assert isinstance(fitted_model.kernel, SupportVectorMachine.Kernel.Linear)
+
+    def test_should_be_passed_to_sklearn(self, training_set: TaggedTable) -> None:
+        kernel = SupportVectorMachine.Kernel.Linear()
+        fitted_model = SupportVectorMachine(c=2, kernel=kernel).fit(training_set)
+        assert fitted_model._wrapped_classifier is not None
+        assert isinstance(fitted_model.kernel, SupportVectorMachine.Kernel.Linear)
+
     def test_should_get_sklearn_kernel_linear(self) -> None:
         svm = SupportVectorMachine(c=2, kernel=SupportVectorMachine.Kernel.Linear())
         assert isinstance(svm.kernel, SupportVectorMachine.Kernel.Linear)
