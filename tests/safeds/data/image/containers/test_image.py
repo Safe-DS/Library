742193eaa5fdf0acdca5d2f5693e8fdf46e7348f
daniaHu <s5dahuss@uni-bonn.de>
2023-06-16 13:08:16 +0200

    flip tests adjusted

--- tests/safeds/data/image/containers/test_image.py
+++ tests/safeds/data/image/containers/test_image.py
@@ -225,3 +225,4 @@ class TestFlipHorizontally:
         image = Image.from_png_file(resolve_resource_path("image/original.png"))
         image2 = image.flip_horizontally().flip_horizontally()
         assert image == image2
+
