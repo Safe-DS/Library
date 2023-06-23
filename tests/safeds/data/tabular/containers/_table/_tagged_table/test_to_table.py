c4185c9f1da5d99e8b22080849ff675482fd79d4
Severin Paul Höfer <84280965+zzril@users.noreply.github.com>
2023-06-23 10:03:51 +0200

    Add test for to_table

---
+++ tests/safeds/data/tabular/containers/_table/_tagged_table/test_to_table.py
@@ -0,0 +1,22 @@
+from safeds.data.tabular.containers import TaggedTable, Table
+
+
+def test_should_return_table() -> None:
+    tagged_table = TaggedTable(
+        {
+            "feature_1": [3, 9, 6],
+            "feature_2": [6, 12, 9],
+            "target": [1, 3, 2],
+        },
+        "target",
+    )
+    expected = Table(
+        {
+            "feature_1": [3, 9, 6],
+            "feature_2": [6, 12, 9],
+            "target": [1, 3, 2],
+        },
+    )
+    table = TaggedTable.to_table(tagged_table)
+    assert table.schema == expected.schema
+    assert table == expected
