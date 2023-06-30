d50a943555d1df9dbb1793cfddfbcd66aa15720d
Severin Paul Höfer <84280965+zzril@users.noreply.github.com>
2023-06-30 18:38:25 +0200

    Update test_rename_column.py

--- tests/safeds/data/tabular/containers/_table/_tagged_table/test_rename_column.py
+++ tests/safeds/data/tabular/containers/_table/_tagged_table/test_rename_column.py
@@ -11,40 +11,70 @@ from tests.helpers import assert_that_tagged_tables_are_equal
             TaggedTable(
                 {
                     "feature_old": [0, 1, 2],
+                    "no_feature": [2, 3, 4],
                     "target": [3, 4, 5],
                 },
-                "target",
+                target_name="target",
+                feature_names=["feature_old"]
             ),
             "feature_old",
             "feature_new",
             TaggedTable(
                 {
                     "feature_new": [0, 1, 2],
+                    "no_feature": [2, 3, 4],
                     "target": [3, 4, 5],
                 },
-                "target",
+                target_name="target",
+                feature_names=["feature_new"]
             ),
         ),
         (
             TaggedTable(
                 {
                     "feature": [0, 1, 2],
+                    "no_feature": [2, 3, 4],
                     "target_old": [3, 4, 5],
                 },
-                "target_old",
+                target_name="target_old",
+                feature_names=["feature"]
             ),
             "target_old",
             "target_new",
             TaggedTable(
                 {
                     "feature": [0, 1, 2],
+                    "no_feature": [2, 3, 4],
                     "target_new": [3, 4, 5],
                 },
-                "target_new",
+                target_name="target_new",
+                feature_names=["feature"]
+            ),
+        ),
+        (
+            TaggedTable(
+                {
+                    "feature": [0, 1, 2],
+                    "no_feature_old": [2, 3, 4],
+                    "target": [3, 4, 5],
+                },
+                target_name="target",
+                feature_names=["feature"]
+            ),
+            "no_feature_old",
+            "no_feature_new",
+            TaggedTable(
+                {
+                    "feature": [0, 1, 2],
+                    "no_feature_new": [2, 3, 4],
+                    "target": [3, 4, 5],
+                },
+                target_name="target",
+                feature_names=["feature"]
             ),
         ),
     ],
-    ids=["rename_feature_column", "rename_target_column"],
+    ids=["rename_feature_column", "rename_target_column", "rename_non_feature_column"],
 )
 def test_should_add_column(
     original_table: TaggedTable,
