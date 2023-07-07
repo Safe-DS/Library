b2c1b299548cb22f18d4618d9b38061357a98b63
methr0 <s6tameth@uni-bonn.de>
2023-07-07 09:24:39 +0200

    requested changes

--- src/safeds/data/tabular/transformation/_discretizer.py
+++ src/safeds/data/tabular/transformation/_discretizer.py
@@ -40,7 +40,7 @@ class Discretizer(TableTransformer):
         ----------
         table : Table
             The table used to fit the transformer.
-        column_names : Optional[list[str]]
+        column_names : list[str] | None
             The list of columns from the table used to fit the transformer. If `None`, all columns are used.
 
         Returns
