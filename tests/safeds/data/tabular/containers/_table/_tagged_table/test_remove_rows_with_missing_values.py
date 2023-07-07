5e3c1d4842f2b2b344a7bef514bac8af3b9e5bfe
Severin Paul Höfer <84280965+zzril@users.noreply.github.com>
2023-07-07 09:51:15 +0200

    Update test_remove_rows_with_missing_values.py

--- tests/safeds/data/tabular/containers/_table/_tagged_table/test_remove_rows_with_missing_values.py
+++ tests/safeds/data/tabular/containers/_table/_tagged_table/test_remove_rows_with_missing_values.py
@@ -1,22 +1,47 @@
+import pytest
 from safeds.data.tabular.containers import TaggedTable
 
 from tests.helpers import assert_that_tagged_tables_are_equal
 
 
-def test_should_remove_row() -> None:
-    table = TaggedTable(
-        {
-            "feature": [0.0, None, 2.0],
-            "target": [3.0, 4.0, 5.0],
-        },
-        "target",
-    )
+@pytest.mark.parametrize(
+    ("table" ,"expected"),
+    [
+        (
+            TaggedTable(
+                {
+                    "feature": [0.0, None, 2.0],
+                    "target": [3.0, 4.0, 5.0],
+                },
+                "target",
+            ),
+            TaggedTable(
+                {
+                    "feature": [0.0, 2.0],
+                    "target": [3.0, 5.0],
+                },
+                "target",
+            )
+        ),
+        (
+            TaggedTable(
+                {
+                    "feature": [0.0, 1.0, 2.0],
+                    "target": [3.0, 4.0, 5.0],
+                },
+                "target",
+            ),
+            TaggedTable(
+                {
+                    "feature": [0.0, 1.0, 2.0],
+                    "target": [3.0, 4.0, 5.0],
+                },
+                "target",
+            )
+        ),
+    ],
+    ids=["with_missing_values", "without_missing_values"]
+)
+def test_should_remove_rows_with_missing_values(table: TaggedTable, expected: TaggedTable) -> None:
     new_table = table.remove_rows_with_missing_values()
-    expected = TaggedTable(
-        {
-            "feature": [0.0, 2.0],
-            "target": [3.0, 5.0],
-        },
-        "target",
-    )
     assert_that_tagged_tables_are_equal(new_table, expected)
