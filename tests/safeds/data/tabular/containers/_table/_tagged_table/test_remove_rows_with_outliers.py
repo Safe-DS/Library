447dd82533d4fb047f7ef28be96bbfa69ad5ede5
Severin Paul Höfer <84280965+zzril@users.noreply.github.com>
2023-07-07 09:36:52 +0200

    Update test_remove_rows_with_outliers.py

--- tests/safeds/data/tabular/containers/_table/_tagged_table/test_remove_rows_with_outliers.py
+++ tests/safeds/data/tabular/containers/_table/_tagged_table/test_remove_rows_with_outliers.py
@@ -1,22 +1,30 @@
+import pytest
 from safeds.data.tabular.containers import TaggedTable
 
 from tests.helpers import assert_that_tagged_tables_are_equal
 
 
-def test_should_remove_row() -> None:
-    table = TaggedTable(
-        {
-            "feature": [1.0, 11.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
-            "target": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
-        },
-        "target",
-    )
+@pytest.mark.parametrize(
+    ("table", "expected"),
+    [
+        (
+            TaggedTable(
+                {
+                    "feature": [1.0, 11.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                    "target": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                },
+                "target",
+            ),
+            TaggedTable(
+                {
+                    "feature": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                    "target": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                },
+                "target",
+            )
+        ),
+    ]
+)
+def test_should_remove_row(table: TaggedTable, expected: TaggedTable) -> None:
     new_table = table.remove_rows_with_outliers()
-    expected = TaggedTable(
-        {
-            "feature": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
-            "target": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
-        },
-        "target",
-    )
     assert_that_tagged_tables_are_equal(new_table, expected)
