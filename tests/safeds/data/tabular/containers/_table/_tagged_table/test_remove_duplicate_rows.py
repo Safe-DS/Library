b352d53bfe9cdd598da6da5564f811a1353dfca3
Severin Paul Höfer <84280965+zzril@users.noreply.github.com>
2023-07-07 09:54:51 +0200

    Update test_remove_duplicate_rows.py

--- tests/safeds/data/tabular/containers/_table/_tagged_table/test_remove_duplicate_rows.py
+++ tests/safeds/data/tabular/containers/_table/_tagged_table/test_remove_duplicate_rows.py
@@ -1,22 +1,47 @@
+import pytest
 from safeds.data.tabular.containers import TaggedTable
 
 from tests.helpers import assert_that_tagged_tables_are_equal
 
 
-def test_should_remove_row() -> None:
-    table = TaggedTable(
-        {
-            "feature": [0, 0, 1],
-            "target": [2, 2, 3],
-        },
-        "target",
-    )
+@pytest.mark.parametrize(
+    ("table", "expected"),
+    [
+        (
+            TaggedTable(
+                {
+                    "feature": [0, 0, 1],
+                    "target": [2, 2, 3],
+                },
+                "target",
+            ),
+            TaggedTable(
+                {
+                    "feature": [0, 1],
+                    "target": [2, 3],
+                },
+                "target",
+            )
+        ),
+        (
+            TaggedTable(
+                {
+                    "feature": [0, 1, 2],
+                    "target": [2, 2, 3],
+                },
+                "target",
+            ),
+            TaggedTable(
+                {
+                    "feature": [0, 1, 2],
+                    "target": [2, 2, 3],
+                },
+                "target",
+            )
+        ),
+    ],
+    ids=["with_duplicate_rows", "without_duplicate_rows"]
+)
+def test_should_remove_duplicate_rows(table: TaggedTable, expected: TaggedTable) -> None:
     new_table = table.remove_duplicate_rows()
-    expected = TaggedTable(
-        {
-            "feature": [0, 1],
-            "target": [2, 3],
-        },
-        "target",
-    )
     assert_that_tagged_tables_are_equal(new_table, expected)
