31c83f3190601a60b9090a609c4c43f082646c33
Severin Paul Höfer <84280965+zzril@users.noreply.github.com>
2023-07-07 09:44:59 +0200

    Add new testcase to test_remove_rows_with_outliers

--- tests/safeds/data/tabular/containers/_table/_tagged_table/test_remove_rows_with_outliers.py
+++ tests/safeds/data/tabular/containers/_table/_tagged_table/test_remove_rows_with_outliers.py
@@ -23,8 +23,25 @@ from tests.helpers import assert_that_tagged_tables_are_equal
                 "target",
             )
         ),
-    ]
+        (
+            TaggedTable(
+                {
+                    "feature": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                    "target": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                },
+                "target",
+            ),
+            TaggedTable(
+                {
+                    "feature": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                    "target": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                },
+                "target",
+            )
+        ),
+    ],
+    ids=["with_outliers", "no_outliers"]
 )
-def test_should_remove_row(table: TaggedTable, expected: TaggedTable) -> None:
+def test_should_remove_rows_with_outliers(table: TaggedTable, expected: TaggedTable) -> None:
     new_table = table.remove_rows_with_outliers()
     assert_that_tagged_tables_are_equal(new_table, expected)
