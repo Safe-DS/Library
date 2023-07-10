6ed099058a450a462d24a9529699a6a9344fcc38
alex-senger <s6alseng@uni-bonn.de>
2023-07-10 18:22:33 +0200

    test: Add test for CodeCov
    Co-authored-by: sibre28 <86068340+sibre28@users.noreply.github.com>

--- tests/safeds/data/tabular/typing/test_column_type.py
+++ tests/safeds/data/tabular/typing/test_column_type.py
@@ -118,3 +118,10 @@ class TestIsNumeric:
     )
     def test_should_return_whether_the_column_type_is_numeric(self, column_type: ColumnType, expected: bool) -> None:
         assert column_type.is_numeric() == expected
+
+
+# We need this test for CodeCoverage
+class TestAbstractClass:
+    def test_should_raise_if_abstract_class_is_initialized(self) -> None:
+        with pytest.raises(TypeError):
+            ColumnType()
