dcd3fb542774f5f973511de545fd2a7c0824a072
daniaHu <s5dahuss@uni-bonn.de>
2023-07-11 15:16:01 +0200

    tested if shuffled table still has same rows by sorting both tables and comparing them

--- tests/safeds/data/tabular/containers/_table/test_shuffle_rows.py
+++ tests/safeds/data/tabular/containers/_table/test_shuffle_rows.py
@@ -1,19 +1,17 @@
-import numpy as np
 import pytest
 from safeds.data.tabular.containers import Table
 
 
 @pytest.mark.parametrize(
-    ("table", "expected"),
+    "table",
     [
-        (Table(), Table()),
-        (Table({"col1": [1, 3, 5], "col2": [2, 4, 6]}), Table({"col1": [5, 1, 3], "col2": [6, 2, 4]})),
-        (Table({"col1": [1], "col2": [2]}), Table({"col1": [1], "col2": [2]})),
+        Table(),
+        Table({"col1": [1, 3, 5], "col2": [2, 4, 6]}),
+        Table({"col1": [1], "col2": [2]}),
     ],
     ids=["Empty table", "Table with multiple rows", "Table with one row"],
 )
-def test_should_shuffle_rows(table: Table, expected: Table) -> None:
-    np.random.RandomState(123456)
+def test_should_shuffle_rows(table: Table) -> None:
     result_table = table.shuffle_rows()
     assert table.schema == result_table.schema
-    assert result_table == expected
+    assert table.sort_rows(lambda row1, row2: row1["col1"] - row2["col1"]) == result_table.sort_rows(lambda row1, row2: row1["col1"] - row2["col1"])
