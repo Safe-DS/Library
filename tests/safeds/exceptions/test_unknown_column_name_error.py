d24b0b5a10cae8ff633ff6a60616b729e00c5ba9
Severin Paul Höfer <84280965+zzril@users.noreply.github.com>
2023-07-13 22:17:26 +0200

    Add testcases

--- tests/safeds/exceptions/test_unknown_column_name_error.py
+++ tests/safeds/exceptions/test_unknown_column_name_error.py
@@ -5,9 +5,21 @@ from safeds.exceptions import UnknownColumnNameError
 @pytest.mark.parametrize(
     ("column_names", "similar_columns", "expected_error_message"),
     [
-        (["column1", "column2"], [], r"Could not find column\(s\) 'column1, column2'."),
+        (["column1"], [], r"Could not find column\(s\) 'column1'\."),
+        (["column1", "column2"], [], r"Could not find column\(s\) 'column1, column2'\."),
+        (["column1"], ["column_a"], r"Could not find column\(s\) 'column1'\.\nDid you mean '\['column_a'\]'\?"),
+        (["column1", "column2"], ["column_a"], r"Could not find column\(s\) 'column1, column2'\.\nDid you mean '\['column_a'\]'\?"),
+        (["column1"], ["column_a", "column_b"], r"Could not find column\(s\) 'column1'\.\nDid you mean '\['column_a', 'column_b'\]'\?"),
+        (["column1", "column2"], ["column_a", "column_b"], r"Could not find column\(s\) 'column1, column2'\.\nDid you mean '\['column_a', 'column_b'\]'\?"),
     ],
-    ids=["empty_list"],
+    ids=[
+        "one_unknown_no_suggestions",
+        "two_unknown_no_suggestions",
+        "one_unknown_one_suggestion",
+        "two_unknown_one_suggestion",
+        "one_unknown_two_suggestions",
+        "two_unknown_two_suggestions",
+    ],
 )
 def test_empty_similar_columns(
     column_names: list[str],
