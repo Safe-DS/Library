fe34fcf67cfc8471cb1a5140a9e91bb62cfe25f2
Severin Paul Höfer <84280965+zzril@users.noreply.github.com>
2023-07-13 20:41:08 +0200

    Fix missing period

--- src/safeds/data/tabular/containers/_tagged_table.py
+++ src/safeds/data/tabular/containers/_tagged_table.py
@@ -525,7 +525,7 @@ class TaggedTable(Table):
 
     def remove_duplicate_rows(self) -> TaggedTable:
         """
-        Return a new `TaggedTable` with all row duplicates removed
+        Return a new `TaggedTable` with all row duplicates removed.
 
         The original table is not modified.
 
