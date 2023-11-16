bf76fda5e22b87fdc4e80c13bc7fafcd2a724bfc
Lars Reimann <mail@larsreimann.com>
2023-11-16 17:43:21 +0100

    test: fixtures for snapshot tests

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+from typing import Any
+
+import matplotlib as mpl
+import pytest
+from syrupy import SnapshotAssertion
+from syrupy.extensions.single_file import SingleFileSnapshotExtension
+from syrupy.types import SerializedData
+
+from safeds.data.image.containers import Image
+
+# Fix for failures when running pytest in a terminal (https://github.com/Safe-DS/Library/issues/482)
+mpl.use("agg")
+
+
+class JPEGImageExtension(SingleFileSnapshotExtension):
+    _file_extension = "jpg"
+
+    def serialize(self, data: Image, **_kwargs: Any) -> SerializedData:
+        return data._repr_jpeg_()
+
+
+@pytest.fixture()
+def snapshot_jpeg(snapshot) -> SnapshotAssertion:
+    return snapshot.use_extension(JPEGImageExtension)
+
+
+class PNGImageSnapshotExtension(SingleFileSnapshotExtension):
+    _file_extension = "png"
+
+    def serialize(self, data: Image, **_kwargs: Any) -> SerializedData:
+        return data._repr_png_()
+
+
+@pytest.fixture()
+def snapshot_png(snapshot) -> SnapshotAssertion:
+    return snapshot.use_extension(PNGImageSnapshotExtension)
