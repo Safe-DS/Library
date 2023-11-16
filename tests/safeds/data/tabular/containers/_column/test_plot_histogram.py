d0eb9e864fd28ff8a2806b4b4150732ac0ea3362
Lars Reimann <mail@larsreimann.com>
2023-11-16 18:55:22 +0100

    test: snapshot tests for column plots

--- tests/safeds/data/tabular/containers/_column/test_plot_histogram.py
+++ tests/safeds/data/tabular/containers/_column/test_plot_histogram.py
@@ -1,21 +1,13 @@
-from safeds.data.image.containers import Image
 from safeds.data.tabular.containers import Table
 
-from tests.helpers import resolve_resource_path
 
-
-def test_should_match_snapshot_numeric() -> None:
+def test_should_match_snapshot_numeric(snapshot_png) -> None:
     table = Table({"A": [1, 2, 3]})
-    current = table.get_column("A").plot_histogram()
-    snapshot = Image.from_png_file(resolve_resource_path("./image/snapshot_histogram_numeric.png"))
-
-    # Inlining the expression into the assert causes pytest to hang if the assertion fails when run from PyCharm.
-    assertion = snapshot._image.tobytes() == current._image.tobytes()
-    assert assertion
+    histogram = table.get_column("A").plot_histogram()
+    assert histogram == snapshot_png
 
 
-def test_should_match_snapshot_str() -> None:
+def test_should_match_snapshot_str(snapshot_png) -> None:
     table = Table({"A": ["A", "B", "Apple"]})
-    current = table.get_column("A").plot_histogram()
-    snapshot = Image.from_png_file(resolve_resource_path("./image/snapshot_histogram_str.png"))
-    assert snapshot._image.tobytes() == current._image.tobytes()
+    histogram = table.get_column("A").plot_histogram()
+    assert histogram == snapshot_png
