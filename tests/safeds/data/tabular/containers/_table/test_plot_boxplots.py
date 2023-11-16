64c3b122040023897199aa0a926c034b9ebdefc1
Lars Reimann <mail@larsreimann.com>
2023-11-16 19:02:53 +0100

    test: snapshot tests for table plots

--- tests/safeds/data/tabular/containers/_table/test_plot_boxplots.py
+++ tests/safeds/data/tabular/containers/_table/test_plot_boxplots.py
@@ -1,33 +1,20 @@
 import pytest
-from safeds.data.image.containers import Image
 from safeds.data.tabular.containers import Table
 from safeds.exceptions import NonNumericColumnError
 
-from tests.helpers import resolve_resource_path
-
 
 @pytest.mark.parametrize(
-    ("table", "path"),
+    "table",
     [
-        (Table({"A": [1, 2, 3]}), "./image/snapshot_boxplots/one_column.png"),
-        (
-            Table({"A": [1, 2, 3], "B": ["A", "A", "Bla"], "C": [True, True, False], "D": [1.0, 2.1, 4.5]}),
-            "./image/snapshot_boxplots/four_columns_some_non_numeric.png",
-        ),
-        (
-            Table({"A": [1, 2, 3], "B": [1.0, 2.1, 4.5], "C": [1, 2, 3], "D": [1.0, 2.1, 4.5]}),
-            "./image/snapshot_boxplots/four_columns_all_numeric.png",
-        ),
+        Table({"A": [1, 2, 3]}),
+        Table({"A": [1, 2, 3], "B": ["A", "A", "Bla"], "C": [True, True, False], "D": [1.0, 2.1, 4.5]}),
+        Table({"A": [1, 2, 3], "B": [1.0, 2.1, 4.5], "C": [1, 2, 3], "D": [1.0, 2.1, 4.5]}),
     ],
     ids=["one column", "four columns (some non-numeric)", "four columns (all numeric)"],
 )
-def test_should_match_snapshot(table: Table, path: str) -> None:
-    current = table.plot_boxplots()
-    snapshot = Image.from_png_file(resolve_resource_path(path))
-
-    # Inlining the expression into the assert causes pytest to hang if the assertion fails when run from PyCharm.
-    assertion = snapshot._image.tobytes() == current._image.tobytes()
-    assert assertion
+def test_should_match_snapshot(table: Table, snapshot_png) -> None:
+    boxplots = table.plot_boxplots()
+    assert boxplots == snapshot_png
 
 
 def test_should_raise_if_column_contains_non_numerical_values() -> None:
--- tests/safeds/data/tabular/containers/_table/test_plot_correlation_heatmap.py
+++ tests/safeds/data/tabular/containers/_table/test_plot_correlation_heatmap.py
@@ -1,29 +1,22 @@
 import pytest
-from safeds.data.image.containers import Image
 from safeds.data.tabular.containers import Table
-
-from tests.helpers import resolve_resource_path
 
 
 @pytest.mark.parametrize(
-    ("table", "path"),
+    "table",
     [
-        (Table({"A": [1, 2, 3.5], "B": [0.2, 4, 77]}), "./image/snapshot_heatmap.png"),
-        (Table(), "./image/snapshot_empty_heatmap.png"),
+        Table({"A": [1, 2, 3.5], "B": [0.2, 4, 77]}),
     ],
-    ids=["normal", "empty"],
+    ids=["normal"],
 )
-def test_should_match_snapshot(table: Table, path: str) -> None:
-    if table.number_of_rows == 0:
-        with pytest.warns(
-            UserWarning,
-            match=r"An empty table has been used. A correlation heatmap on an empty table will show nothing.",
-        ):
-            current = table.plot_correlation_heatmap()
-    else:
-        current = table.plot_correlation_heatmap()
-    snapshot = Image.from_png_file(resolve_resource_path(path))
+def test_should_match_snapshot(table: Table, snapshot_png) -> None:
+    correlation_heatmap = table.plot_correlation_heatmap()
+    assert correlation_heatmap == snapshot_png
 
-    # Inlining the expression into the assert causes pytest to hang if the assertion fails when run from PyCharm.
-    assertion = snapshot == current
-    assert assertion
+
+def test_should_warn_about_empty_table() -> None:
+    with pytest.warns(
+        UserWarning,
+        match=r"An empty table has been used. A correlation heatmap on an empty table will show nothing.",
+    ):
+        Table().plot_correlation_heatmap()
