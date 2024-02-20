d1f06fbd0b6c6de24f795589ae1a109658787adb
Gerhardsa0 <andreasgerhards@t-online.de>
2024-02-20 15:16:56 +0100

    comment fixed

--- src/safeds/data/tabular/containers/_time_series.py
+++ src/safeds/data/tabular/containers/_time_series.py
@@ -884,8 +884,8 @@ class TimeSeries(TaggedTable):
                 >>> from safeds.data.tabular.containers import TimeSeries
                 >>> table = TimeSeries({"time":[1, 2], "target": [3, 4]}, target_name= "target", time_name="time", feature_names=None, )
                 >>> image = table.plot_lag_plot(lag = 1)
-                """
-        Examples
+
+         Examples
         --------
         >>> from safeds.data.tabular.containers import TimeSeries
         >>> table = TimeSeries({"time":[1, 2], "target": [3, 4]}, target_name= "target", time_name="time")
