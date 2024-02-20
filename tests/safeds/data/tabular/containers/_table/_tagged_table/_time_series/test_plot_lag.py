b7d31d8206e117bc49e16731a2f14c57dfcd4496
Gerhardsa0 <andreasgerhards@t-online.de>
2024-02-20 14:52:17 +0100

    changed name and added numerical error if target contains non numerical values

--- tests/safeds/data/tabular/containers/_table/_tagged_table/_time_series/test_plot_lag.py
+++ tests/safeds/data/tabular/containers/_table/_tagged_table/_time_series/test_plot_lag.py
@@ -1,7 +1,7 @@
 import pytest
 from safeds.data.tabular.containers import Table, TimeSeries
 from syrupy import SnapshotAssertion
-from safeds.exceptions import IllegalSchemaModificationError
+from safeds.exceptions import IllegalSchemaModificationError, NonNumericColumnError
 
 from tests.helpers import assert_that_time_series_are_equal
 
@@ -16,5 +16,25 @@ def test_should_return_table(snapshot_png: SnapshotAssertion) -> None:
         target_name="target",
         time_name="time",
         feature_names=None, )
-    lag_plot = table.plot_lag(lag=1)
+    lag_plot = table.plot_lag_plot(lag=1)
     assert lag_plot == snapshot_png
+
+def test_should_raise_if_column_contains_non_numerical_values() -> None:
+    table = TimeSeries(
+        {
+            "time": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            "feature_1": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            "target": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+        },
+        target_name="target",
+        time_name="time",
+        feature_names=None, )
+    with pytest.raises(
+        NonNumericColumnError,
+        match=(
+            r"Tried to do a numerical operation on one or multiple non-numerical columns: \nThis time series target contains"
+            r" non-numerical columns."
+        ),
+    ):
+        table.plot_lagplot(2)
+
