efb5074d97822c5092d7936c289d945e4d5cd1cf
Gerhardsa0 <andreasgerhards@t-online.de>
2024-03-20 13:44:59 +0100

    updated test

--- tests/safeds/ml/classical/regression/test_arimaModel.py
+++ tests/safeds/ml/classical/regression/test_arimaModel.py
@@ -3,17 +3,21 @@ from safeds.ml.classical.regression import ArimaModel
 from safeds.data.tabular.containers import TimeSeries, Table
 from syrupy import SnapshotAssertion
 import numpy as np
+import os
+
+from tests.helpers import resolve_resource_path
 
 
 def test_arima_model(snapshot_png: SnapshotAssertion) -> None:
     # Create a DataFrame
+    _inflation_path = "_datas/US_Inflation_rates.csv"
     np.random.seed(42)
-    time_series = TimeSeries.timeseries_from_csv_file("C:/Users/ettel/PycharmProjects/Library/tests/resources/_datas/US_Inflation_rates.csv",                                                  target_name= "value", time_name="date")
+    time_series = TimeSeries.timeseries_from_csv_file(path=resolve_resource_path(_inflation_path), target_name= "value", time_name="date")
     train_ts, test_ts = time_series.split_rows(0.8)
     model = ArimaModel()
     trained_model = model.fit(train_ts)
     predictions = trained_model.predict(test_ts)
+    print(os.getcwd)
     print(predictions)
     assert snapshot_png == trained_model.plot_predictions(test_ts)
-
 
