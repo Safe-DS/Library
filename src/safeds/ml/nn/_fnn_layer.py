1487ea28cf3755edf18fdbaaecec5ccee1f2cd95
Simon <s6snbreu@uni-bonn.de>
2024-04-10 16:27:31 +0200

    change for linter

--- src/safeds/ml/nn/_fnn_layer.py
+++ src/safeds/ml/nn/_fnn_layer.py
@@ -32,6 +32,7 @@ class Layer(ABC):
     def _get_internal_layer(self, activation_function: str) -> _InternalLayer:
         pass  # pragma: no cover
 
+    @property
     @abstractmethod
     def output_size(self) -> int:
         pass  # pragma: no cover
