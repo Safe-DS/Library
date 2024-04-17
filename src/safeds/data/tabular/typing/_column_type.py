bf1da18a8f528282766b81a999bcf9af5de88212
Lars Reimann <mail@larsreimann.com>
2024-04-17 15:24:51 +0200

    style: fix ruff errors

--- src/safeds/data/tabular/typing/_column_type.py
+++ src/safeds/data/tabular/typing/_column_type.py
@@ -48,9 +48,9 @@ class ColumnType(ABC):
         import numpy as np
 
         def column_type_of_type(cell_type: Any) -> ColumnType:
-            if cell_type == int or cell_type == np.int64 or cell_type == np.int32:
+            if cell_type in (int, np.int64, np.int32):
                 return Integer(is_nullable)
-            if cell_type == float or cell_type == np.float64 or cell_type == np.float32:
+            if cell_type in (float, np.float64, np.float32):
                 return RealNumber(is_nullable)
             if cell_type == bool:
                 return Boolean(is_nullable)
