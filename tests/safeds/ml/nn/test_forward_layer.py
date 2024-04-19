c205410e0c38886ea27998da1b71e4668db50035
Simon <s6snbreu@uni-bonn.de>
2024-04-19 17:33:09 +0200

    add tests

--- tests/safeds/ml/nn/test_forward_layer.py
+++ tests/safeds/ml/nn/test_forward_layer.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Any
 
 import pytest
 from safeds.exceptions import OutOfBoundsError
@@ -75,31 +76,39 @@ def test_should_raise_if_output_size_doesnt_match(output_size: int) -> None:
 
 
 @pytest.mark.parametrize(
-    ("layer1", "layer2"),
+    ("layer1", "layer2", "equal"),
     [
         (
             ForwardLayer(input_size=1, output_size=2),
             ForwardLayer(input_size=1, output_size=2),
+            True,
         ),
-    ],
-    ids=["equal"],
-)
-def test_should_assert_that_forward_layers_are_equal(layer1: ForwardLayer, layer2: ForwardLayer) -> None:
-    assert layer1.__eq__(layer2)
-
-
-@pytest.mark.parametrize(
-    ("layer1", "layer2"),
-    [
         (
             ForwardLayer(input_size=1, output_size=2),
             ForwardLayer(input_size=2, output_size=1),
+            False,
         ),
     ],
-    ids=["not equal"],
+    ids=["equal", "not equal"],
 )
-def test_should_assert_that_forward_layers_are_not_equal(layer1: ForwardLayer, layer2: ForwardLayer) -> None:
-    assert not layer1.__eq__(layer2)
+def test_should_compare_forward_layers(layer1: ForwardLayer, layer2: ForwardLayer, equal: bool) -> None:
+    assert (layer1.__eq__(layer2)) == equal
+
+
+def test_should_assert_that_forward_layer_is_equal_to_itself() -> None:
+    layer = ForwardLayer(input_size=1, output_size=1)
+    assert layer.__eq__(layer)
+
+
+@pytest.mark.parametrize(
+    ("layer", "other"),
+    [
+        (ForwardLayer(input_size=1, output_size=1), None),
+    ],
+    ids=["ForwardLayer vs. None"],
+)
+def test_should_return_not_implemented_if_other_is_not_table(layer: ForwardLayer, other: Any) -> None:
+    assert (layer.__eq__(other)) is NotImplemented
 
 
 @pytest.mark.parametrize(
@@ -126,7 +135,8 @@ def test_should_assert_that_equal_forward_layers_have_equal_hash(layer1: Forward
     ],
     ids=["not equal"],
 )
-def test_should_assert_that_different_forward_layers_have_different_hash(layer1: ForwardLayer, layer2: ForwardLayer) -> None:
+def test_should_assert_that_different_forward_layers_have_different_hash(layer1: ForwardLayer,
+                                                                         layer2: ForwardLayer) -> None:
     assert layer1.__hash__() != layer2.__hash__()
 
 
