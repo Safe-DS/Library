1fdfbeeb87eee00db78bd658247213a16830903e
Alexander Gréus <alexgreus51@gmail.com>
2024-04-24 20:15:09 +0200

    build(deps): Bump pytorch to 2.3.0 and torchvision to 0.18.0

--- src/safeds/data/image/containers/_image.py
+++ src/safeds/data/image/containers/_image.py
@@ -428,7 +428,7 @@ class Image:
                 device=self.device,
             )
         else:
-            return Image(func2.rgb_to_grayscale(self._image_tensor[0:3], num_output_channels=3), device=self.device)
+            return Image(func2.rgb_to_grayscale(self._image_tensor[0:3], num_output_channels=self.channel), device=self.device)
 
     def crop(self, x: int, y: int, width: int, height: int) -> Image:
         """
