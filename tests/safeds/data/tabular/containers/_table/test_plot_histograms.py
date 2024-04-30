d711d0a8dc95d2b1c394c25124f2ea827ae7e628
Saman Hushi <s6sahush@uni-bonn.de>
2024-04-30 15:49:04 +0200

    added new case for Test plot_histograms() - now also tests more than 10 unique numerical and string values

--- tests/safeds/data/tabular/containers/_table/test_plot_histograms.py
+++ tests/safeds/data/tabular/containers/_table/test_plot_histograms.py
@@ -15,8 +15,14 @@ from syrupy import SnapshotAssertion
                 "D": [1.0, 2.1, 2.1, 2.1, 2.1, 3.0, 3.0],
             },
         ),
+        Table(
+            {
+                "A": [3.8, 1.8, 3.2, 2.2, 1.0, 2.4, 3.5, 3.9, 1.9, 4.0, 1.4, 4.2, 4.5, 4.5, 1.4, 2.5, 2.8, 2.8, 1.9, 4.3],
+                "B": ["a", "b", "b", "c", "d", "f", "a", "f", "e", "a", "b", "b", "k", "j", "b", "i", "h", "g", "g", "a"],
+            }
+        ),
     ],
-    ids=["one column", "four columns"],
+    ids=["one column", "four columns", "two columns with compressed visualization"],
 )
 def test_should_match_snapshot(table: Table, snapshot_png_image: SnapshotAssertion) -> None:
     histograms = table.plot_histograms()
