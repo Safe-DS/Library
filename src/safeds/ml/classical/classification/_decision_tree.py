102de2d90a129d33c78f58597d590df1f0da3ad3
Lars Reimann <mail@larsreimann.com>
2024-05-05 22:38:41 +0200

    feat: regularization for decision trees and random forests (#730)
    Closes #700
    
    ### Summary of Changes
    
    Add regularization options for decision trees and random forests:
    * maximum depth
    * minimum number of samples in leaves

--- src/safeds/ml/classical/classification/_decision_tree.py
+++ src/safeds/ml/classical/classification/_decision_tree.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from safeds._utils import _structural_hash
+from safeds.exceptions import ClosedBound, OutOfBoundsError
 from safeds.ml.classical._util_sklearn import fit, predict
 
 from ._classifier import Classifier
@@ -16,16 +17,65 @@ if TYPE_CHECKING:
 
 
 class DecisionTreeClassifier(Classifier):
-    """Decision tree classification."""
+    """
+    Decision tree classification.
 
-    def __hash__(self) -> int:
-        return _structural_hash(Classifier.__hash__(self), self._target_name, self._feature_names)
+    Parameters
+    ----------
+    maximum_depth:
+        The maximum depth of each tree. If None, the depth is not limited. Has to be greater than 0.
+    minimum_number_of_samples_in_leaves:
+        The minimum number of samples that must remain in the leaves of each tree. Has to be greater than 0.
 
-    def __init__(self) -> None:
+    Raises
+    ------
+    OutOfBoundsError
+        If `maximum_depth` is less than 1.
+    OutOfBoundsError
+        If `minimum_number_of_samples_in_leaves` is less than 1.
+    """
+
+    def __init__(
+        self,
+        *,
+        maximum_depth: int | None = None,
+        minimum_number_of_samples_in_leaves: int = 1,
+    ) -> None:
+        # Validation
+        if maximum_depth is not None and maximum_depth < 1:
+            raise OutOfBoundsError(maximum_depth, name="maximum_depth", lower_bound=ClosedBound(1))
+        if minimum_number_of_samples_in_leaves < 1:
+            raise OutOfBoundsError(
+                minimum_number_of_samples_in_leaves,
+                name="minimum_number_of_samples_in_leaves",
+                lower_bound=ClosedBound(1),
+            )
+
+        # Hyperparameters
+        self._maximum_depth: int | None = maximum_depth
+        self._minimum_number_of_samples_in_leaves: int = minimum_number_of_samples_in_leaves
+
         # Internal state
         self._wrapped_classifier: sk_DecisionTreeClassifier | None = None
         self._feature_names: list[str] | None = None
         self._target_name: str | None = None
+
+    def __hash__(self) -> int:
+        return _structural_hash(
+            Classifier.__hash__(self),
+            self._feature_names,
+            self._target_name,
+        )
+
+    @property
+    def maximum_depth(self) -> int | None:
+        """The maximum depth of the tree."""
+        return self._maximum_depth
+
+    @property
+    def minimum_number_of_samples_in_leaves(self) -> int:
+        """The minimum number of samples that must remain in the leaves of the tree."""
+        return self._minimum_number_of_samples_in_leaves
 
     def fit(self, training_set: TabularDataset) -> DecisionTreeClassifier:
         """
@@ -59,7 +109,10 @@ class DecisionTreeClassifier(Classifier):
         wrapped_classifier = self._get_sklearn_classifier()
         fit(wrapped_classifier, training_set)
 
-        result = DecisionTreeClassifier()
+        result = DecisionTreeClassifier(
+            maximum_depth=self._maximum_depth,
+            minimum_number_of_samples_in_leaves=self._minimum_number_of_samples_in_leaves,
+        )
         result._wrapped_classifier = wrapped_classifier
         result._feature_names = training_set.features.column_names
         result._target_name = training_set.target.name
@@ -105,4 +158,7 @@ class DecisionTreeClassifier(Classifier):
     def _get_sklearn_classifier(self) -> ClassifierMixin:
         from sklearn.tree import DecisionTreeClassifier as sk_DecisionTreeClassifier
 
-        return sk_DecisionTreeClassifier()
+        return sk_DecisionTreeClassifier(
+            max_depth=self._maximum_depth,
+            min_samples_leaf=self._minimum_number_of_samples_in_leaves,
+        )
