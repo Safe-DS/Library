3e4409d367228f67a34e03f0c3292eb34afb892e
Gerhardsa0 <andreasgerhards@t-online.de>
2024-05-07 13:59:17 +0200

    linter changes

--- tests/safeds/data/labeled/containers/_time_series_dataset/test_into_dataloader.py
+++ tests/safeds/data/labeled/containers/_time_series_dataset/test_into_dataloader.py
@@ -25,8 +25,8 @@ from torch.utils.data import DataLoader
 )
 def test_should_create_dataloader(
     data: dict[str, list[int]],
-    target_name: int,
-    time_name: int,
+    target_name: str,
+    time_name: str,
     extra_names: list[str] | None,
 ) -> None:
     tabular_dataset = Table.from_dict(data).to_time_series_dataset(target_name, time_name, extra_names)
@@ -81,8 +81,8 @@ def test_should_create_dataloader(
 )
 def test_should_create_dataloader_invalid(
     data: TimeSeriesDataset,
-    window_size: str,
-    forecast_horizon: str,
+    window_size: int,
+    forecast_horizon: int,
     error_type: ValueError,
     error_msg: str,
 ) -> None:
