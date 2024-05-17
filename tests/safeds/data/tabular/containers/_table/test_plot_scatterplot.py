6a2a040b8a871545308c4290877f289ee50c5220
Lars Reimann <mail@larsreimann.com>
2024-05-17 15:53:38 +0200

    feat: outline around dots of scatterplot

--- tests/safeds/data/tabular/containers/_table/test_plot_scatterplot.py
+++ tests/safeds/data/tabular/containers/_table/test_plot_scatterplot.py
@@ -1,12 +1,30 @@
 import pytest
 from safeds.data.tabular.containers import Table
-from safeds.exceptions import ColumnNotFoundError
+from safeds.exceptions import ColumnNotFoundError, ColumnTypeError
 from syrupy import SnapshotAssertion
 
 
-def test_should_match_snapshot(snapshot_png_image: SnapshotAssertion) -> None:
-    table = Table({"A": [1, 2, 3], "B": [2, 4, 7]})
-    scatterplot = table.plot.scatter_plot("A", "B")
+@pytest.mark.parametrize(
+    ("table", "x_name", "y_name"),
+    [
+        (Table({"A": [1, 2, 3], "B": [2, 4, 7]}), "A", "B"),
+        (
+            Table(
+                {
+                    "A": [1, 0.99, 0.99, 2],
+                    "B": [1, 0.99, 1.01, 2],
+                 }),
+            "A",
+            "B",
+        ),
+    ],
+    ids=[
+        "functional",
+        "overlapping",
+    ],
+)
+def test_should_match_snapshot(table: Table, x_name: str, y_name: str, snapshot_png_image: SnapshotAssertion) -> None:
+    scatterplot = table.plot.scatter_plot(x_name, y_name)
     assert scatterplot == snapshot_png_image
 
 
@@ -23,3 +41,14 @@ def test_should_match_snapshot(snapshot_png_image: SnapshotAssertion) -> None:
 def test_should_raise_if_column_does_not_exist(table: Table, col1: str, col2: str) -> None:
     with pytest.raises(ColumnNotFoundError):
         table.plot.scatter_plot(col1, col2)
+
+@pytest.mark.parametrize(
+    ("table", "x_name", "y_name"),
+    [
+        (Table({"A": ["a", "b", "c"], "B": [2, 4, 7]}), "A", "B"),
+        (Table({"A": [1, 2, 3], "B": ["a", "b", "c"]}), "A", "B"),
+    ],
+)
+def test_should_raise_if_columns_are_not_numeric(table: Table, x_name: str, y_name: str) -> None:
+    with pytest.raises(ColumnTypeError):
+        table.plot.scatter_plot(x_name, y_name)
