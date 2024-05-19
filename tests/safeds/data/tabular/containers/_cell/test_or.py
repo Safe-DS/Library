a1dba019f4a627293a95f3c68851a45a93cae6de
Lars Reimann <mail@larsreimann.com>
2024-05-19 11:56:07 +0200

    test: `Cell.or_`

---
+++ tests/safeds/data/tabular/containers/_cell/test_or.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+import pytest
+
+from tests.helpers import assert_cell_operation_works
+
+
+@pytest.mark.parametrize(
+    ("value1", "value2", "expected"),
+    [
+        (False, False, False),
+        (False, True, True),
+        (True, False, True),
+        (True, True, True),
+        (0, False, False),
+        (0, True, True),
+        (1, False, True),
+        (1, True, True),
+    ],
+    ids=[
+        "false - false",
+        "false - true",
+        "true - false",
+        "true - true",
+        "falsy int - false",
+        "falsy int - true",
+        "truthy int - false",
+        "truthy int - true",
+    ],
+)
+class TestShouldComputeDisjunction:
+    def test_dunder_method(self, value1: Any, value2: bool, expected: bool):
+        assert_cell_operation_works(value1, lambda cell: cell | value2, expected)
+
+    def test_dunder_method_inverted_order(self, value1: Any, value2: bool, expected: bool):
+        assert_cell_operation_works(value1, lambda cell: value2 | cell, expected)
+
+    def test_named_method(self, value1: Any, value2: bool, expected: bool):
+        assert_cell_operation_works(value1, lambda cell: cell.or_(value2), expected)
