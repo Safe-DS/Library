58fe6b307f5e9adb6e4efe20583223b413f9fbe0
Lars Reimann <mail@larsreimann.com>
2024-05-20 16:24:26 +0200

    refactor: remove `_get_output_configuration`

--- src/safeds/ml/nn/converters/_input_converter_image_to_column.py
+++ src/safeds/ml/nn/converters/_input_converter_image_to_column.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 from safeds._config import _init_default_device
 from safeds.data.image.containers._single_size_image_list import _SingleSizeImageList
 from safeds.data.labeled.containers import ImageDataset
 from safeds.data.labeled.containers._image_dataset import _ColumnAsTensor
 from safeds.data.tabular.containers import Column
-from safeds.data.tabular.transformation import OneHotEncoder
 
 from ._input_converter_image import _InputConversionImage
 
@@ -15,6 +14,7 @@ if TYPE_CHECKING:
     from torch import Tensor
 
     from safeds.data.image.containers import ImageList
+    from safeds.data.tabular.transformation import OneHotEncoder
 
 
 class InputConversionImageToColumn(_InputConversionImage):
@@ -22,7 +22,6 @@ class InputConversionImageToColumn(_InputConversionImage):
         self,
         input_data: ImageList,
         output_data: Tensor,
-        **kwargs: Any,
     ) -> ImageDataset[Column]:
         import torch
 
@@ -30,16 +29,16 @@ class InputConversionImageToColumn(_InputConversionImage):
 
         if not isinstance(input_data, _SingleSizeImageList):
             raise ValueError("The given input ImageList contains images of different sizes.")  # noqa: TRY004
-        if "column_name" not in kwargs or not isinstance(kwargs.get("column_name"), str):
+        if self._column_name is None:
             raise ValueError(
                 "The column_name is not set. The data can only be converted if the column_name is provided as `str` in the kwargs.",
             )
-        if "one_hot_encoder" not in kwargs or not isinstance(kwargs.get("one_hot_encoder"), OneHotEncoder):
+        if self._one_hot_encoder is None:
             raise ValueError(
                 "The one_hot_encoder is not set. The data can only be converted if the one_hot_encoder is provided as `OneHotEncoder` in the kwargs.",
             )
-        one_hot_encoder: OneHotEncoder = kwargs["one_hot_encoder"]
-        column_name: str = kwargs["column_name"]
+        one_hot_encoder: OneHotEncoder = self._one_hot_encoder
+        column_name: str = self._column_name
 
         output = torch.zeros(len(input_data), len(one_hot_encoder._get_names_of_added_columns()))
         output[torch.arange(len(input_data)), output_data] = 1
