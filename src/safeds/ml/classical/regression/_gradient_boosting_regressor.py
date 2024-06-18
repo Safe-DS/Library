92859f98e4fefd8bf724c2d4f5f77f13d1ad8b39
Simon <s6snbreu@uni-bonn.de>
2024-06-18 16:26:28 +0200

    add gradient boosting regression

--- src/safeds/ml/classical/regression/_gradient_boosting_regressor.py
+++ src/safeds/ml/classical/regression/_gradient_boosting_regressor.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Self
 
 from safeds._utils import _structural_hash
+from safeds.data.labeled.containers import TabularDataset
+from safeds.exceptions import FittingWithChoiceError, FittingWithoutChoiceError
 from safeds.ml.classical._bases import _GradientBoostingBase
 
 from ._regressor import Regressor
+from ...hyperparameters import Choice
 
 if TYPE_CHECKING:
     from sklearn.base import RegressorMixin
@@ -37,8 +40,8 @@ class GradientBoostingRegressor(Regressor, _GradientBoostingBase):
     def __init__(
         self,
         *,
-        tree_count: int = 100,
-        learning_rate: float = 0.1,
+        tree_count: int | Choice[int] = 100,
+        learning_rate: float | Choice[float] = 0.1,
     ) -> None:
         # Initialize superclasses
         Regressor.__init__(self)
@@ -71,3 +74,23 @@ class GradientBoostingRegressor(Regressor, _GradientBoostingBase):
             n_estimators=self._tree_count,
             learning_rate=self._learning_rate,
         )
+
+    def _check_additional_fit_preconditions(self, training_set: TabularDataset) -> None:
+        if isinstance(self._tree_count, Choice) or isinstance(self._learning_rate, Choice):
+            raise FittingWithChoiceError
+
+    def _check_additional_fit_by_exhaustive_search_preconditions(self, training_set: TabularDataset) -> None:
+        if not isinstance(self._tree_count, Choice) and not isinstance(self._learning_rate, Choice):
+            raise FittingWithoutChoiceError
+
+    def _get_models_for_all_choices(self) -> list[Self]:
+        tree_count_choices = self._tree_count if isinstance(self._tree_count, Choice) else [
+            self._tree_count]
+        learning_rate_choices = self._learning_rate if isinstance(self._learning_rate, Choice) else [
+            self._learning_rate]
+
+        models = []
+        for tc in tree_count_choices:
+            for lr in learning_rate_choices:
+                models.append(GradientBoostingRegressor(tree_count=tc, learning_rate=lr))
+        return models
