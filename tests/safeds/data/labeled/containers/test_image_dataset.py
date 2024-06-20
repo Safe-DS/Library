121b99fd7612d24db4a834998103b22fb0a4a12d
Alexander Gréus <alexgreus51@gmail.com>
2024-06-20 21:34:09 +0200

    feat: add `ImageDataset.split`

--- tests/safeds/data/labeled/containers/test_image_dataset.py
+++ tests/safeds/data/labeled/containers/test_image_dataset.py
@@ -382,6 +382,104 @@ class TestBatch:
 
 
 @pytest.mark.parametrize("device", get_devices(), ids=get_devices_ids())
+@pytest.mark.parametrize(
+    "shuffle",
+    [
+        True,
+        False
+    ]
+)
+class TestSplit:
+
+    @pytest.mark.parametrize(
+        "output",
+        [
+            Column("images", images_all()[:4] + images_all()[5:]),
+            Table({"0": [1, 0, 0, 0, 0, 0], "1": [0, 1, 0, 0, 0, 0], "2": [0, 0, 1, 0, 0, 0], "3": [0, 0, 0, 1, 0, 0], "4": [0, 0, 0, 0, 1, 0], "5": [0, 0, 0, 0, 0, 1]}),
+            _EmptyImageList(),
+        ],
+        ids=["Column", "Table", "ImageList"]
+    )
+    def test_should_split(self, device: Device, shuffle: bool, output: Column | Table | ImageList) -> None:
+        configure_test_with_device(device)
+        image_list = ImageList.from_files(resolve_resource_path(images_all())).remove_duplicate_images().resize(10, 10)
+        if isinstance(output, _EmptyImageList):
+            output = image_list
+        image_dataset = ImageDataset(image_list, output)
+        image_dataset1, image_dataset2 = image_dataset.split(0.4, shuffle=shuffle)
+        offset = len(image_dataset1)
+        assert len(image_dataset1) == round(0.4 * len(image_dataset))
+        assert len(image_dataset2) == len(image_dataset) - offset
+        assert len(image_dataset1.get_input()) == round(0.4 * len(image_dataset))
+        assert len(image_dataset2.get_input()) == len(image_dataset) - offset
+        if isinstance(image_dataset.get_output(), Table):
+            assert image_dataset1.get_output().row_count == round(0.4 * len(image_dataset))
+            assert image_dataset2.get_output().row_count == len(image_dataset) - offset
+        else:
+            assert len(image_dataset1.get_output()) == round(0.4 * len(image_dataset))
+            assert len(image_dataset2.get_output()) == len(image_dataset) - offset
+
+        assert image_dataset != image_dataset1
+        assert image_dataset != image_dataset2
+        assert image_dataset1 != image_dataset2
+
+        for i, image in enumerate(image_dataset1.get_input().to_images()):
+            index = image_list.index(image)[0]
+            if not shuffle:
+                assert index == i
+            if isinstance(image_dataset1.get_output(), ImageList):
+                assert image_list.index(image_dataset1.get_output().get_image(i))[0] == index
+            elif isinstance(image_dataset1.get_output(), Column):
+                assert output.to_list().index(image_dataset1.get_output().to_list()[i]) == index
+            elif isinstance(image_dataset1.get_output(), Table):
+                assert output.get_column(str(index)).to_list()[index] == 1
+
+        for i, image in enumerate(image_dataset2.get_input().to_images()):
+            index = image_list.index(image)[0]
+            if not shuffle:
+                assert index == i + offset
+            if isinstance(image_dataset2.get_output(), ImageList):
+                assert image_list.index(image_dataset2.get_output().get_image(i))[0] == index
+            elif isinstance(image_dataset2.get_output(), Column):
+                assert output.to_list().index(image_dataset2.get_output().to_list()[i]) == index
+            elif isinstance(image_dataset2.get_output(), Table):
+                assert output.get_column(str(index)).to_list()[index] == 1
+
+        image_dataset._batch_size = len(image_dataset)
+        image_dataset1._batch_size = 1
+        image_dataset2._batch_size = 1
+        image_dataset_batch = next(iter(image_dataset))
+
+        for i, b in enumerate(iter(image_dataset1)):
+            assert b[0] in image_dataset_batch[0]
+            index = (b[0] == image_dataset_batch[0]).all(dim=[1, 2, 3]).nonzero()[0][0]
+            if not shuffle:
+                assert index == i
+            assert torch.all(torch.eq(b[0], image_dataset_batch[0][index]))
+            assert torch.all(torch.eq(b[1], image_dataset_batch[1][index]))
+
+        for i, b in enumerate(iter(image_dataset2)):
+            assert b[0] in image_dataset_batch[0]
+            index = (b[0] == image_dataset_batch[0]).all(dim=[1, 2, 3]).nonzero()[0][0]
+            if not shuffle:
+                assert index == i + offset
+            assert torch.all(torch.eq(b[0], image_dataset_batch[0][index]))
+            assert torch.all(torch.eq(b[1], image_dataset_batch[1][index]))
+
+    @pytest.mark.parametrize(
+        "percentage",
+        [-1, -0.1, 1.1, 2]
+    )
+    def test_should_raise(self, device: Device, shuffle: bool, percentage: float):
+        configure_test_with_device(device)
+        image_list = ImageList.from_files(resolve_resource_path(images_all())).resize(10, 10)
+        image_dataset = ImageDataset(image_list, Column("images", images_all()))
+        with pytest.raises(OutOfBoundsError):
+            image_dataset.split(percentage, shuffle=shuffle)
+
+
+
+@pytest.mark.parametrize("device", get_devices(), ids=get_devices_ids())
 class TestTableAsTensor:
     def test_should_raise_if_not_one_hot_encoded(self, device: Device) -> None:
         configure_test_with_device(device)
