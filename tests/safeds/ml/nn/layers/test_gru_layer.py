ab29e9d7c854795919f6bad825d1ac9eb902029e
Gerhardsa0 <andreasgerhards@t-online.de>
2024-06-20 18:37:02 +0200

    linter fixes

--- tests/safeds/ml/nn/layers/test_gru_layer.py
+++ tests/safeds/ml/nn/layers/test_gru_layer.py
@@ -177,5 +177,9 @@ def test_input_size_should_raise_error() -> None:
     layer = GRULayer(1)
     layer._input_size = None
     with pytest.raises(ValueError, match="The input_size is not yet set.", ):
-        layer.input_size
+        layer.input_size()
 
+def test_internal_layer_should_raise_error() -> None:# noqa
+    layer = GRULayer(1)
+    with pytest.raises(ValueError, match="The input_size is not yet set."):  # noqa
+        layer._get_internal_layer(activation_function="relu")  # noqa
