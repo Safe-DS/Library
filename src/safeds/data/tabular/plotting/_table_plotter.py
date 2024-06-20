3ffc6403f1ac339369a15dfc65199068986ea866
Gerhardsa0 <andreasgerhards@t-online.de>
2024-06-20 20:28:15 +0200

    linter changes and better error message

--- src/safeds/data/tabular/plotting/_table_plotter.py
+++ src/safeds/data/tabular/plotting/_table_plotter.py
@@ -429,11 +429,12 @@ def _plot_validation(table: Table, x_name: str, y_names: list[str]) -> None:
     _check_columns_exist(table, y_names)
     y_names.remove(x_name)
     _check_columns_are_numeric(table, y_names)
-    y_names.append(x_name)
 
     if not table.get_column(x_name).is_numeric and not table.get_column(x_name).is_temporal:
         raise ColumnTypeError(x_name)
+    y_names.append(x_name)
     for name in y_names:
         if table.get_column(name).missing_value_count() >= 1:
             raise ValueError(f"there are missing values in column '{name}', use transformation to fill missing values "
                              f"or drop the missing values")
+    y_names.remove(x_name)
