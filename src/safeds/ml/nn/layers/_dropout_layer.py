c6fccd799e23c596fa10af99b37ddf6b78f26953
Tim Locke <tim.hennef@gmail.com>
2024-06-21 16:13:45 +0200

    hopefully finishes DropoutLayer

--- src/safeds/ml/nn/layers/_dropout_layer.py
+++ src/safeds/ml/nn/layers/_dropout_layer.py
@@ -31,7 +31,6 @@ class DropoutLayer(Layer):
         _check_bounds("propability", propability, lower_bound=_OpenBound(0), upper_bound=_OpenBound(1))
         self.propability = propability
         self._input_size: int | None = None
-        self._output_size: int | None = None
 
     def _get_internal_layer(self, **kwargs: Any) -> nn.Module:
         from ._internal_layers import _InternalDropoutLayer  # slow import on global level
@@ -45,3 +44,55 @@ class DropoutLayer(Layer):
                 "The output_size is not yet set. The internal layer can only be created when the output_size is set.",
             )
         return _InternalDropoutLayer(self.propability)
+    
+    @property
+    def input_size(self) -> int:
+        """
+        Get the input_size of this layer.
+
+        Returns
+        -------
+        result:
+            The amount of values being passed into this layer.
+
+        Raises
+        ------
+        ValueError
+            If the input_size is not yet set
+        """
+        if self._input_size is None:
+            raise ValueError("The input_size is not yet set.")
+        return self._input_size
+    
+    @property
+    def output_size(self) -> int:
+        """
+        Get the output_size of this layer.
+
+        Returns
+        -------
+        result:
+            The amount of values being passed out of this layer.
+
+        Raises
+        ------
+        ValueError
+            If the input_size is not yet set
+        """
+        if self._input_size is None:
+            raise ValueError("The input_size is not yet set.")
+        return self._input_size
+    
+    def _set_input_size(self, input_size: int) -> None:
+        self._input_size = input_size
+
+    def __hash__(self) -> int:
+        return _structural_hash(self._input_size)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, DropoutLayer):
+            return NotImplemented
+        return (self is other) or (self._input_size == other._input_size)
+
+    def __sizeof__(self) -> int:
+        return self._input_size
