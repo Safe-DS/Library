f2c0c7f4929722d89e5a3cc356276d2bf201ceae
Sardar <160326696+saius02@users.noreply.github.com>
2024-06-21 15:40:42 +0200

    fertig

--- tests/safeds/data/tabular/containers/_table/test_remove_columns.py
+++ tests/safeds/data/tabular/containers/_table/test_remove_columns.py
@@ -3,29 +3,6 @@ from safeds.data.tabular.containers import Table
 from safeds.exceptions import ColumnNotFoundError
 
 
-# @pytest.mark.parametrize(
-#     ("table", "expected", "columns", "ignore_unknown_names", "should_raise"),
-#     [
-#         (Table({"col1": [1, 2, 1], "col2": ["a", "b", "c"]}), Table({"col1": [1, 2, 1]}), ["col2"]),
-#         (Table({"col1": [1, 2, 1], "col2": [1, 2, 4]}), Table(), ["col1", "col2"]),
-#         (Table({"col1": [1, 2, 1], "col2": [1, 2, 4]}), Table({"col1": [1, 2, 1], "col2": [1, 2, 4]}), []),
-#         (Table(), Table(), []),
-#         (Table(), Table(), ["col1"]),
-#     ],
-#     ids=[
-#         "one column",
-#         "multiple columns",
-#         "no columns",
-#         "empty",
-#         "missing columns",
-#     ],
-# )
-# def test_should_remove_table_columns(table: Table, expected: Table, columns: list[str]) -> None:
-#     table = table.remove_columns(columns)
-#     assert table.schema == expected.schema
-#     assert table == expected
-#     assert table.row_count == expected.row_count
-
 @pytest.mark.parametrize(
     ("table", "expected", "columns", "ignore_unknown_names", "should_raise"),
     [
