dd1300d2ce5a165213bf2a0cbb8c6e31bd19cacf
gitgott1 <tim.wirtz04@gmail.com>
2024-06-28 15:15:26 +0200

    added plot for DecisionTreeClassifier

--- tests/safeds/ml/classical/classification/test_decision_tree.py
+++ tests/safeds/ml/classical/classification/test_decision_tree.py
@@ -1,8 +1,9 @@
 import pytest
 from safeds.data.labeled.containers import TabularDataset
 from safeds.data.tabular.containers import Table
-from safeds.exceptions import OutOfBoundsError
+from safeds.exceptions import OutOfBoundsError, ModelNotFittedError
 from safeds.ml.classical.classification import DecisionTreeClassifier
+from syrupy import SnapshotAssertion
 
 
 @pytest.fixture()
@@ -41,3 +42,15 @@ class TestMinSampleCountInLeaves:
     def test_should_raise_if_less_than_or_equal_to_0(self, min_sample_count_in_leaves: int) -> None:
         with pytest.raises(OutOfBoundsError):
             DecisionTreeClassifier(min_sample_count_in_leaves=min_sample_count_in_leaves)
+            
+class TestPlot:
+    def test_should_raise_if_model_is_not_fittet(self) -> None:
+        model = DecisionTreeClassifier()
+        with pytest.raises(ModelNotFittedError):
+            model.plot()
+        
+    def test_should_check_that_plot_image_is_same_as_plt_figure(self, training_set: TabularDataset, snapshot_png_image: SnapshotAssertion) -> None:
+        fitted_model = DecisionTreeClassifier().fit(training_set)
+        image = fitted_model.plot()
+        assert image == snapshot_png_image
+    
