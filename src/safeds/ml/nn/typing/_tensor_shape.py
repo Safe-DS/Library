790ed89c4a79f22352b079a899d01c6c12a5bf29
Tim Locke <tlc@objektkultur.de>
2024-07-12 16:12:03 +0200

    fixes something again, hopefully

--- src/safeds/ml/nn/typing/_tensor_shape.py
+++ src/safeds/ml/nn/typing/_tensor_shape.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from typing import Self
-
 from safeds._utils import _structural_hash
 from safeds._validation import _check_bounds, _ClosedBound
 
@@ -20,8 +18,7 @@ class TensorShape:
     def __init__(self, dims: list[int]) -> None:
         self._dims = dims 
 
-    @classmethod
-    def get_size(cls: type[Self], dimension: int | None = None) -> int:
+    def get_size(self, dimension: int | None = None) -> int:
         """
         Return the size of the tensor in the specified dimension.
 
@@ -30,21 +27,26 @@ class TensorShape:
         dimension:
             The dimension index for which the size is to be retrieved.
 
-        Return
+        Returns
         -------
             int: The size of the tensor in the specified dimension.
+
+        Raises
+        ------
+        OutOfBoundsError:
+            If the actual value is outside its expected range.
         """
         _check_bounds("dimension",dimension, lower_bound=_ClosedBound(0))
-        if(dimension >= cls.dimensionality):
+        if dimension is not None and dimension >= self.dimensionality:
             #TODO maybe add error message indicating that the dimension is out of range
             return 0        
         if(dimension is None):
-            return cls._dims
-        return cls._dims[dimension]
-     
+            return self._dims
+        return self._dims[dimension]
+    
     def __hash__(self) -> int:
         return _structural_hash(self._dims)
-
+     
     @property
     def dimensionality(self) -> int:
         """
