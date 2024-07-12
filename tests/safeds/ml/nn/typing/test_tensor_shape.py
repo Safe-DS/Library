1fd480792f3b9a0ff9cbaf3a35d58b8fe8d6d421
peplaul0 <s6lepepl@uni-bonn.de>
2024-07-12 15:08:31 +0200

    pytests written

---
+++ tests/safeds/ml/nn/typing/test_tensor_shape.py
@@ -0,0 +1,38 @@
+import pytest
+from safeds.exceptions import OutOfBoundsError
+from safeds.ml.nn.typing import TensorShape
+
+
+def test_dims():
+        dims = [2, 3, 4]
+        ts = TensorShape(dims)
+        assert ts._dims == dims
+
+def test_dimensionality():
+        dims = [2, 3, 4]
+        ts = TensorShape(dims)
+        assert ts.dimensionality == 3
+
+def test_get_size_no_dimension():
+    dims = [2, 3, 4]
+    ts = TensorShape(dims)
+    assert ts.get_size() == dims
+
+def test_get_size_specific_dimension():
+    dims = [2, 3, 4]
+    ts = TensorShape(dims)
+    assert ts.get_size(0) == 2
+    assert ts.get_size(1) == 3
+    assert ts.get_size(2) == 4
+
+def test_get_size_out_of_bounds_dimension():
+    dims = [2, 3, 4]
+    ts = TensorShape(dims)
+    with pytest.raises(OutOfBoundsError):
+        ts.get_size(-1)
+
+def test_hash():
+    dims = [2, 3, 4]
+    ts1 = TensorShape(dims)
+    ts2 = TensorShape(dims)
+    assert hash(ts1) == hash(ts2)
