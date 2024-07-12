03caaf69a5cb4937df25556cb09417ff8e2f102d
srose <118634249+wastedareas@users.noreply.github.com>
2024-07-12 11:24:52 +0200

    All tests passed

--- tests/safeds/data/tabular/containers/_cell/test_first_not_none.py
+++ tests/safeds/data/tabular/containers/_cell/test_first_not_none.py
@@ -6,7 +6,7 @@ from safeds.data.tabular.containers._lazy_cell import _LazyCell
 
 
 class TestFirstNotNone:
-    def test_should_return_none() -> None:
+    def test_should_return_none(self) -> None:
         to_eval = [_LazyCell(None) for i in range(5)]
         res = Cell.first_not_none(to_eval)
         assert(res.eq(_LazyCell(None)))
@@ -19,9 +19,9 @@ class TestFirstNotNone:
             ([_LazyCell(None), _LazyCell(None), _LazyCell("Hello, World!"), _LazyCell("Not returned")], _LazyCell("Hello, World!")),
             ([_LazyCell(i) for i in ["a", "b", "c", "d"]], _LazyCell("a")),
             ([_LazyCell(i) for i in [None, time(0,0,0,0), None, time(1, 1, 1, 1)]], _LazyCell(time(0,0,0,0))),
-            ([_LazyCell(time(i,i,i,i) for i in range(4))], _LazyCell(time(0,0,0,0))),
+            ([_LazyCell(i) for i in [time(0,0,0,0), time(1,1,1,1), time(2,2,2,2), time(3,3,3,3)]], _LazyCell(time(0,0,0,0))),
             ([_LazyCell(i) for i in [None, date(2000, 1, 1), date(1098, 3, 4), None]], _LazyCell(date(2000, 1, 1))),
-            ([_LazyCell(date(2000, 3, i)) for i in range(5)], _LazyCell(date(2000, 3, 1))),
+            ([_LazyCell(date(2000, 3, i)) for i in range(1, 5)], _LazyCell(date(2000, 3, 1))),
             ([_LazyCell(i) for i in [None, "a", 1, time(0,0,0,0)]], _LazyCell("a")),
             ([_LazyCell(i) for i in [time(1,1,1,1), 0, "c", date(2020, 1, 7)]], _LazyCell(time(1,1,1,1))),
         ],
@@ -42,7 +42,7 @@ class TestFirstNotNone:
         res = Cell.first_not_none(list_of_cells)
         assert(res.eq(expected))
 
-    def test_given_empty_list() -> None:
+    def test_given_empty_list(self) -> None:
         to_eval = []
         res = Cell.first_not_none(to_eval)
         assert(res.eq(_LazyCell(None)))
