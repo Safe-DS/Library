133e832d64363d0de66d7028e7140817dafd0ca5
Lars Reimann <mail@larsreimann.com>
2024-07-12 14:36:55 +0200

    test: homogeneous data

--- tests/safeds/data/tabular/containers/_table/test_sizeof.py
+++ tests/safeds/data/tabular/containers/_table/test_sizeof.py
@@ -9,7 +9,7 @@ from safeds.data.tabular.containers import Table
     [
         Table(),
         Table({"col1": [0]}),
-        Table({"col1": [0, "1"], "col2": ["a", "b"]}),
+        Table({"col1": [0, 1], "col2": ["a", "b"]}),
     ],
     ids=[
         "empty table",
