eab9833b873ff40f28dd8da5f348566f52b5a752
Camilla Grefrath <s23cgref@uni-bonn.de>
2024-07-12 14:06:36 +0200

    tests for validation

--- tests/safeds/data/tabular/containers/_table/test_join.py
+++ tests/safeds/data/tabular/containers/_table/test_join.py
@@ -58,3 +58,26 @@ def test_join(
     table_expected: Table,
 ) -> None:
     assert table_left.join(table_right, left_names, right_names, mode=mode) == table_expected
+
+def test_join_mismatched_columns() -> None:
+    table_left = Table({"a": [1, 2], "b": [3, 4]})
+    table_right = Table({"d": [1, 5], "e": [5, 6]})
+    left_names = ["a"]
+    right_names = ["d", "e"]
+    mode = "inner"
+    with pytest.raises(ValueError, match="The number of columns to join on must be the same in both tables."):
+        table_left.join(table_right, left_names, right_names, mode)
+
+
+def test_join_check_columns_exist() -> None:
+    table_left = Table({"a": [1, 2], "b": [3, 4]})
+    table_right = Table({"d": [1, 5], "e": [5, 6]})
+    left_names = ["a", "c"]
+    right_names = ["d", "e"]
+    mode = "inner"
+    with pytest.raises(KeyError):
+        table_left.join(table_right, left_names, right_names, mode)
+    left_names = ["a"]
+    right_names = ["d", "f"]
+    with pytest.raises(KeyError):
+        table_left.join(table_right, left_names, right_names, mode)
