0a487637ed9365d8c585f7dce5129b09f12500c1
Simon Breuer <86068340+sibre28@users.noreply.github.com>
2024-07-12 16:46:01 +0200

    Apply suggestions from code review
    Co-authored-by: Lars Reimann <mail@larsreimann.com>

--- src/safeds/ml/classical/classification/_baseline_classifier.py
+++ src/safeds/ml/classical/classification/_baseline_classifier.py
@@ -2,7 +2,7 @@ import copy
 from concurrent.futures import ALL_COMPLETED, wait
 from typing import Self
 
-from joblib._multiprocessing_helpers import mp
+import multiprocessing as mp
 
 from safeds._validation._check_columns_are_numeric import _check_columns_are_numeric
 from safeds.data.labeled.containers import TabularDataset
