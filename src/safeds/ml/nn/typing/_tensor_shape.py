0510fdaccb7a5e5870fae25599f1a10e491ab2e0
peplaul0 <s6lepepl@uni-bonn.de>
2024-07-12 15:00:22 +0200

    src file now updated

--- src/safeds/ml/nn/typing/_tensor_shape.py
+++ src/safeds/ml/nn/typing/_tensor_shape.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
-import sys
-from abc import ABC, abstractmethod
 from typing import Self
 
 from safeds._utils import _structural_hash
 from safeds._validation import _check_bounds, _ClosedBound
 
 
-class TensorShape(ABC):
+class TensorShape:
     """
     Initializes a TensorShape object with the given dimensions.
 
@@ -22,8 +20,7 @@ class TensorShape(ABC):
     def __init__(self, dims: list[int]) -> None:
         self._dims = dims 
 
-    @classmethod
-    def get_size(cls: type[Self], dimension: int | None = None) -> int:
+    def get_size(self, dimension: int | None = None) -> int:
         """
         Return the size of the tensor in the specified dimension.
 
@@ -32,19 +29,23 @@ class TensorShape(ABC):
         dimension:
             The dimension index for which the size is to be retrieved.
 
-        Return
+        Returns
         -------
             int: The size of the tensor in the specified dimension.
+
+        Raises
+        ------
+        OutOfBoundsError:
+            If the actual value is outside its expected range.
         """
         _check_bounds("dimension",dimension, lower_bound=_ClosedBound(0))
-        if(dimension >= cls.dimensionality):
+        if dimension is not None and dimension >= self.dimensionality:
             #TODO maybe add error message indicating that the dimension is out of range
             return 0        
         if(dimension is None):
-            return cls._dims
-        return cls._dims[dimension]
+            return self._dims
+        return self._dims[dimension]
     
-    @abstractmethod
     def __hash__(self) -> int:
         return _structural_hash(self._dims)
      
