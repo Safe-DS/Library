533a966624bbac2deeb84491d743f84d01512e50
Lars Reimann <mail@larsreimann.com>
2024-09-26 13:16:59 +0200

    fix: unwrap operators of cell operations (#932)
    ### Summary of Changes
    
    Always unwrap the right operators of cell operations. This was missing
    in some cases, leading to runtime errors akin to
    
    ```cannot create expression literal for value of type _LazyCell: <Expr ['[(col("Shortness of breath on …'] at 0x18E78C3E010>```
    
    ---------
    
    Co-authored-by: megalinter-bot <129584137+megalinter-bot@users.noreply.github.com>

--- tests/safeds/data/tabular/containers/_cell/test_and.py
+++ tests/safeds/data/tabular/containers/_cell/test_and.py
@@ -1,6 +1,8 @@
 from typing import Any
 
+import polars as pl
 import pytest
+from safeds.data.tabular.containers._lazy_cell import _LazyCell
 
 from tests.helpers import assert_cell_operation_works
 
@@ -32,8 +34,17 @@ class TestShouldComputeConjunction:
     def test_dunder_method(self, value1: Any, value2: bool, expected: bool) -> None:
         assert_cell_operation_works(value1, lambda cell: cell & value2, expected)
 
+    def test_dunder_method_wrapped_in_cell(self, value1: Any, value2: bool, expected: bool) -> None:
+        assert_cell_operation_works(value1, lambda cell: cell & _LazyCell(pl.lit(value2)), expected)
+
     def test_dunder_method_inverted_order(self, value1: Any, value2: bool, expected: bool) -> None:
         assert_cell_operation_works(value2, lambda cell: value1 & cell, expected)
 
+    def test_dunder_method_inverted_order_wrapped_in_cell(self, value1: Any, value2: bool, expected: bool) -> None:
+        assert_cell_operation_works(value2, lambda cell: _LazyCell(pl.lit(value1)) & cell, expected)
+
     def test_named_method(self, value1: Any, value2: bool, expected: bool) -> None:
         assert_cell_operation_works(value1, lambda cell: cell.and_(value2), expected)
+
+    def test_named_method_wrapped_in_cell(self, value1: Any, value2: bool, expected: bool) -> None:
+        assert_cell_operation_works(value1, lambda cell: cell.and_(_LazyCell(pl.lit(value2))), expected)
--- tests/safeds/data/tabular/containers/_cell/test_div.py
+++ tests/safeds/data/tabular/containers/_cell/test_div.py
@@ -1,4 +1,6 @@
+import polars as pl
 import pytest
+from safeds.data.tabular.containers._lazy_cell import _LazyCell
 
 from tests.helpers import assert_cell_operation_works
 
@@ -22,8 +24,17 @@ class TestShouldComputeDivision:
     def test_dunder_method(self, value1: float, value2: float, expected: float) -> None:
         assert_cell_operation_works(value1, lambda cell: cell / value2, expected)
 
+    def test_dunder_method_wrapped_in_cell(self, value1: float, value2: float, expected: float) -> None:
+        assert_cell_operation_works(value1, lambda cell: cell / _LazyCell(pl.lit(value2)), expected)
+
     def test_dunder_method_inverted_order(self, value1: float, value2: float, expected: float) -> None:
         assert_cell_operation_works(value2, lambda cell: value1 / cell, expected)
 
+    def test_dunder_method_inverted_order_wrapped_in_cell(self, value1: float, value2: float, expected: float) -> None:
+        assert_cell_operation_works(value2, lambda cell: _LazyCell(pl.lit(value1)) / cell, expected)
+
     def test_named_method(self, value1: float, value2: float, expected: float) -> None:
         assert_cell_operation_works(value1, lambda cell: cell.div(value2), expected)
+
+    def test_named_method_wrapped_in_cell(self, value1: float, value2: float, expected: float) -> None:
+        assert_cell_operation_works(value1, lambda cell: cell.div(_LazyCell(pl.lit(value2))), expected)
