afafd439505f0eb939617bf640b42be222dc5950
Lars Reimann <mail@larsreimann.com>
2024-11-26 17:13:28 +0100

    fix: index tensor on cpu (#961)
    ### Summary of Changes
    
    The index tensor of an `ImageDataset` sometimes ended up on the CPU,
    instead of the default device, which led to runtime errors. This PR
    fixes this.

--- src/safeds/ml/nn/converters/_input_converter_image_to_column.py
+++ src/safeds/ml/nn/converters/_input_converter_image_to_column.py
@@ -6,7 +6,6 @@ from safeds._config import _init_default_device
 from safeds.data.image.containers._single_size_image_list import _SingleSizeImageList
 from safeds.data.labeled.containers import ImageDataset
 from safeds.data.labeled.containers._image_dataset import _ColumnAsTensor
-from safeds.data.tabular.containers import Column
 
 from ._input_converter_image import _InputConversionImage
 
@@ -14,6 +13,7 @@ if TYPE_CHECKING:
     from torch import Tensor
 
     from safeds.data.image.containers import ImageList
+    from safeds.data.tabular.containers import Column
     from safeds.data.tabular.transformation import OneHotEncoder
 
 
@@ -43,9 +43,9 @@ class InputConversionImageToColumn(_InputConversionImage):
         output = torch.zeros(len(input_data), len(one_hot_encoder._get_names_of_added_columns()))
         output[torch.arange(len(input_data)), output_data] = 1
 
-        im_dataset: ImageDataset[Column] = ImageDataset[Column].__new__(ImageDataset)
+        im_dataset: ImageDataset[Column] = object.__new__(ImageDataset)
         im_dataset._output = _ColumnAsTensor._from_tensor(output, column_name, one_hot_encoder)
-        im_dataset._shuffle_tensor_indices = torch.LongTensor(list(range(len(input_data))))
+        im_dataset._shuffle_tensor_indices = torch.arange(len(input_data))
         im_dataset._shuffle_after_epoch = False
         im_dataset._batch_size = 1
         im_dataset._next_batch_index = 0
--- src/safeds/ml/nn/converters/_input_converter_image_to_table.py
+++ src/safeds/ml/nn/converters/_input_converter_image_to_table.py
@@ -6,7 +6,6 @@ from safeds._config import _init_default_device
 from safeds.data.image.containers._single_size_image_list import _SingleSizeImageList
 from safeds.data.labeled.containers import ImageDataset
 from safeds.data.labeled.containers._image_dataset import _TableAsTensor
-from safeds.data.tabular.containers import Table
 
 from ._input_converter_image import _InputConversionImage
 
@@ -14,6 +13,7 @@ if TYPE_CHECKING:
     from torch import Tensor
 
     from safeds.data.image.containers import ImageList
+    from safeds.data.tabular.containers import Table
 
 
 class InputConversionImageToTable(_InputConversionImage):
@@ -33,9 +33,9 @@ class InputConversionImageToTable(_InputConversionImage):
         output = torch.zeros(len(input_data), len(column_names))
         output[torch.arange(len(input_data)), output_data] = 1
 
-        im_dataset: ImageDataset[Table] = ImageDataset[Table].__new__(ImageDataset)
+        im_dataset: ImageDataset[Table] = object.__new__(ImageDataset)
         im_dataset._output = _TableAsTensor._from_tensor(output, column_names)
-        im_dataset._shuffle_tensor_indices = torch.LongTensor(list(range(len(input_data))))
+        im_dataset._shuffle_tensor_indices = torch.arange(len(input_data))
         im_dataset._shuffle_after_epoch = False
         im_dataset._batch_size = 1
         im_dataset._next_batch_index = 0
