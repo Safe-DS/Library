2d71068676965da235739351f841f012ca6486ba
Lars Reimann <mail@larsreimann.com>
2025-01-09 18:55:46 +0100

    chore: check `__str__`

--- tests/safeds/data/tabular/containers/_table/test_str.py
+++ tests/safeds/data/tabular/containers/_table/test_str.py
@@ -6,16 +6,8 @@ from safeds.data.tabular.containers import Table
     ("table", "expected"),
     [
         (
-            Table({"col1": [1, 2, 1], "col2": [1, 2, 4]}),
-            "+------+------+\n"
-            "| col1 | col2 |\n"
-            "|  --- |  --- |\n"
-            "|  i64 |  i64 |\n"
-            "+=============+\n"
-            "|    1 |    1 |\n"
-            "|    2 |    2 |\n"
-            "|    1 |    4 |\n"
-            "+------+------+",
+            Table({}),
+            "++\n++\n++",
         ),
         (
             Table({"col1": [], "col2": []}),
@@ -27,21 +19,22 @@ from safeds.data.tabular.containers import Table
             "+------+------+",
         ),
         (
-            Table({}),
-            "++\n++\n++",
-        ),
-        (
-            Table({"col1": [1], "col2": [1]}),
+            Table({"col1": [1, 2], "col2": [3, 4]}),
             "+------+------+\n"
             "| col1 | col2 |\n"
             "|  --- |  --- |\n"
             "|  i64 |  i64 |\n"
             "+=============+\n"
-            "|    1 |    1 |\n"
+            "|    1 |    3 |\n"
+            "|    2 |    4 |\n"
             "+------+------+",
         ),
     ],
-    ids=["multiple rows", "rowless table", "empty table", "one row"],
+    ids=[
+        "empty",
+        "no rows",
+        "with data",
+    ],
 )
 def test_should_return_a_string_representation(table: Table, expected: str) -> None:
     assert str(table) == expected
