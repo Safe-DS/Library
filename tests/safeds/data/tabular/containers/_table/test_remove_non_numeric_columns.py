5285a02ffc2f20c37165733d4f259d3d0f298054
Lars Reimann <mail@larsreimann.com>
2025-01-10 16:31:56 +0100

    chore: check `remove_non_numeric_columns`

--- tests/safeds/data/tabular/containers/_table/test_remove_non_numeric_columns.py
+++ tests/safeds/data/tabular/containers/_table/test_remove_non_numeric_columns.py
@@ -1,27 +1,55 @@
+from collections.abc import Callable
+
 import pytest
 from safeds.data.tabular.containers import Table
 
 
 @pytest.mark.parametrize(
-    ("table", "expected"),
+    ("table_factory", "expected"),
     [
         (
-            Table(
-                {
-                    "col1": ["text", "text", "word", "word"],
-                    "col3": [2, 3, 1, 4],
-                },
-            ),
-            Table(
-                {
-                    "col3": [2, 3, 1, 4],
-                },
-            ),
+            lambda: Table({}),
+            Table({}),
         ),
-        (Table({}), Table({})),
+        (
+            lambda: Table({"col1": []}),
+            Table({}),
+        ),
+        (
+            lambda: Table({"col1": [1, 2], "col2": [4, 5]}),
+            Table({"col1": [1, 2], "col2": [4, 5]}),
+        ),
+        (
+            lambda: Table({"col1": [1, 2], "col2": ["a", "b"]}),
+            Table({"col1": [1, 2]}),
+        ),
+        (
+            lambda: Table({"col1": ["a", "b"], "col2": ["a", "b"]}),
+            Table({}),
+        ),
     ],
-    ids=["numerical values", "empty"],
+    ids=[
+        "empty",
+        "no rows",
+        "only numeric columns",
+        "some non-numeric columns",
+        "only non-numeric columns",
+    ],
 )
-def test_should_remove_non_numeric_columns(table: Table, expected: Table) -> None:
-    actual = table.remove_non_numeric_columns()
-    assert actual == expected
+class TestHappyPath:
+    def test_should_remove_non_numeric_columns(
+        self,
+        table_factory: Callable[[], Table],
+        expected: Table,
+    ) -> None:
+        actual = table_factory().remove_non_numeric_columns()
+        assert actual == expected
+
+    def test_should_not_mutate_receiver(
+        self,
+        table_factory: Callable[[], Table],
+        expected: Table,  # noqa: ARG002
+    ) -> None:
+        original = table_factory()
+        original.remove_non_numeric_columns()
+        assert original == table_factory()
