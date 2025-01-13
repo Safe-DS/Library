9f0abcb144870b2fc0bc91a3ca564bdcfeb90476
Lars Reimann <mail@larsreimann.com>
2025-01-13 14:43:50 +0100

    chore: check `max`

--- tests/safeds/data/tabular/containers/_column/test_max.py
+++ tests/safeds/data/tabular/containers/_column/test_max.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 
 from safeds.data.tabular.containers import Column
@@ -6,18 +8,40 @@ from safeds.data.tabular.containers import Column
 @pytest.mark.parametrize(
     ("values", "expected"),
     [
-        ([], None),
-        ([1, 2, 3], 3),
-        ([1, 2, 3, None], 3),
-        ([None, None], None),
+        (
+            [],
+            None,
+        ),
+        (
+            [None, None],
+            None,
+        ),
+        (
+            [1, 2, None],
+            2,
+        ),
+        (
+            [datetime.time(1, 2, 3), datetime.time(4, 5, 6), None],
+            datetime.time(4, 5, 6),
+        ),
+        (
+            ["a", "b", None],
+            "b",
+        ),
+        (
+            [True, False, None],
+            True,
+        ),
     ],
     ids=[
         "empty",
-        "no missing values",
-        "some missing values",
-        "only missing values",
+        "null column",
+        "numeric column",
+        "temporal column",
+        "string column",
+        "boolean column",
     ],
 )
-def test_should_return_max_value(values: list, expected: int) -> None:
-    column = Column("col", values)
+def test_should_return_maximum(values: list, expected: int) -> None:
+    column = Column("col1", values)
     assert column.max() == expected
