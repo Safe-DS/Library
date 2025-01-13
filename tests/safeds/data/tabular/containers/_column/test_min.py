c16e972c5554ff00c9f4b126be494c314330cd4f
Lars Reimann <mail@larsreimann.com>
2025-01-13 14:42:56 +0100

    chore: check `min`

--- tests/safeds/data/tabular/containers/_column/test_min.py
+++ tests/safeds/data/tabular/containers/_column/test_min.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 
 from safeds.data.tabular.containers import Column
@@ -6,18 +8,40 @@ from safeds.data.tabular.containers import Column
 @pytest.mark.parametrize(
     ("values", "expected"),
     [
-        ([], None),
-        ([1, 2, 3], 1),
-        ([1, 2, 3, None], 1),
-        ([None, None], None),
+        (
+            [],
+            None,
+        ),
+        (
+            [None, None],
+            None,
+        ),
+        (
+            [1, 2, None],
+            1,
+        ),
+        (
+            [datetime.time(1, 2, 3), datetime.time(4, 5, 6), None],
+            datetime.time(1, 2, 3),
+        ),
+        (
+            ["a", "b", None],
+            "a",
+        ),
+        (
+            [True, False, None],
+            False,
+        ),
     ],
     ids=[
         "empty",
-        "no missing values",
-        "some missing values",
-        "only missing values",
+        "null column",
+        "numeric column",
+        "temporal column",
+        "string column",
+        "boolean column",
     ],
 )
-def test_should_return_max_value(values: list, expected: int) -> None:
-    column = Column("col", values)
+def test_should_return_minimum(values: list, expected: int) -> None:
+    column = Column("col1", values)
     assert column.min() == expected
