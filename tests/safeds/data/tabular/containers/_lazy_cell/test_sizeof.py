20299891392722fef74ca5e8ebf691fd191aad6f
Lars Reimann <mail@larsreimann.com>
2025-01-15 15:59:15 +0100

    chore: check `__str__` and `__repr__`

--- tests/safeds/data/tabular/containers/_lazy_cell/test_sizeof.py
+++ tests/safeds/data/tabular/containers/_lazy_cell/test_sizeof.py
@@ -1,14 +1,26 @@
 import sys
-from typing import TYPE_CHECKING, Any
 
 import polars as pl
+import pytest
 
+from safeds.data.tabular.containers import Cell
 from safeds.data.tabular.containers._lazy_cell import _LazyCell
 
-if TYPE_CHECKING:
-    from safeds.data.tabular.containers import Cell
 
-
-def test_should_return_size_greater_than_normal_object() -> None:
-    cell: Cell[Any] = _LazyCell(pl.col("a"))
+@pytest.mark.parametrize(
+    "cell",
+    [
+        Cell.constant(1),
+        Cell.date(2025, 1, 15),
+        Cell.date(_LazyCell(pl.col("a")), 1, 15),
+        _LazyCell(pl.col("a")),
+    ],
+    ids=[
+        "constant",
+        "date, int",
+        "date, column",
+        "column",
+    ],
+)
+def test_should_be_larger_than_normal_object(cell: Cell) -> None:
     assert sys.getsizeof(cell) > sys.getsizeof(object())
