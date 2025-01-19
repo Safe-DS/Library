a7a4ad1d593bc35047baf2119527d5a2abd43eaf
Lars Reimann <mail@larsreimann.com>
2025-01-19 16:43:51 +0100

    test: `dt.to_string`

--- tests/safeds/data/tabular/query/_lazy_datetime_operations/test_to_string.py
+++ tests/safeds/data/tabular/query/_lazy_datetime_operations/test_to_string.py
@@ -2,10 +2,10 @@ from datetime import UTC, date, datetime, time
 
 import pytest
 from helpers import assert_cell_operation_works
-from polars.polars import ComputeError, PanicException
 
 from safeds.data.tabular.containers import Column
 from safeds.data.tabular.typing import ColumnType
+from safeds.exceptions import LazyComputationError
 
 DATETIME = datetime(1, 2, 3, 4, 5, 6, 7, tzinfo=UTC)
 DATE = date(1, 2, 3)
@@ -13,74 +13,205 @@ TIME = time(4, 5, 6, 7)
 
 
 @pytest.mark.parametrize(
-    ("value", "format_", "expected"),
+    ("value", "expected"),
     [
-        (datetime(1, 2, 3, 4, 5, 6, 7), "iso", "0001-02-03T04:05:06.000007"),  # noqa: DTZ001
-        (DATETIME, "iso", "0001-02-03T04:05:06.000007+00:00"),
-        (None, "iso", None),
+        (datetime(1, 2, 3, 4, 5, 6, 7), "0001-02-03T04:05:06.000007"),  # noqa: DTZ001
+        (DATETIME, "0001-02-03T04:05:06.000007+00:00"),
+        (DATE, "0001-02-03"),
+        (TIME, "04:05:06.000007"),
+        (None, None),
     ],
     ids=[
-        "iso, no time zone",
-        "iso, with time zone",
+        "datetime without time zone",
+        "datetime with time zone",
+        "date",
+        "time",
         "None",
     ],
 )
-def test_should_stringify_datetime(value: datetime | None, format_: str, expected: str | None):
+def test_should_handle_iso_8601(value: datetime | date | time | None, expected: str | None):
     assert_cell_operation_works(
         value,
-        lambda cell: cell.dt.to_string(format=format_),
+        lambda cell: cell.dt.to_string(format="iso"),
         expected,
         type_if_none=ColumnType.datetime(),
     )
 
 
 @pytest.mark.parametrize(
-    ("value", "format_", "expected"),
+    "value",
     [
-        (DATE, "iso", "0001-02-03"),
-        (None, "iso", None),
+        DATETIME,
+        DATE,
     ],
     ids=[
-        "iso",
-        "None",
+        "datetime",
+        "date",
     ],
 )
-def test_should_stringify_date(value: date | None, format_: str, expected: str | None):
-    assert_cell_operation_works(
-        value,
-        lambda cell: cell.dt.to_string(format=format_),
-        expected,
-        type_if_none=ColumnType.date(),
+class TestDateSpecifiers:
+    @pytest.mark.parametrize(
+        ("format_", "expected"),
+        [
+            ("{Y}", "0001"),
+            ("{_Y}", "   1"),
+            ("{^Y}", "1"),
+            ("{Y99}", "01"),
+            ("{_Y99}", " 1"),
+            ("{^Y99}", "1"),
+            ("{M}", "02"),
+            ("{_M}", " 2"),
+            ("{^M}", "2"),
+            ("{M-full}", "February"),
+            ("{M-short}", "Feb"),
+            ("{W}", "05"),
+            ("{_W}", " 5"),
+            ("{^W}", "5"),
+            ("{D}", "03"),
+            ("{_D}", " 3"),
+            ("{^D}", "3"),
+            ("{DOW}", "6"),
+            ("{DOW-full}", "Saturday"),
+            ("{DOW-short}", "Sat"),
+            ("{DOY}", "034"),
+            ("{_DOY}", " 34"),
+            ("{^DOY}", "34"),
+        ],
+        ids=[
+            "{Y}",
+            "{_Y}",
+            "{^Y}",
+            "{Y99}",
+            "{_Y99}",
+            "{^Y99}",
+            "{M}",
+            "{_M}",
+            "{^M}",
+            "{M-full}",
+            "{M-short}",
+            "{W}",
+            "{_W}",
+            "{^W}",
+            "{D}",
+            "{_D}",
+            "{^D}",
+            "{DOW}",
+            "{DOW-full}",
+            "{DOW-short}",
+            "{DOY}",
+            "{_DOY}",
+            "{^DOY}",
+        ],
     )
+    def test_should_be_replaced_with_correct_string(self, value: datetime | date, format_: str, expected: str) -> None:
+        assert_cell_operation_works(
+            value,
+            lambda cell: cell.dt.to_string(format=format_),
+            expected,
+        )
 
 
 @pytest.mark.parametrize(
-    ("value", "format_", "expected"),
+    "value",
     [
-        (TIME, "iso", "04:05:06.000007"),
-        (None, "iso", None),
+        DATETIME,
+        TIME,
     ],
     ids=[
-        "iso",
-        "None",
+        "datetime",
+        "time",
     ],
 )
-def test_should_stringify_time(value: time | None, format_: str, expected: str | None):
-    assert_cell_operation_works(
-        value,
-        lambda cell: cell.dt.to_string(format=format_),
-        expected,
-        type_if_none=ColumnType.time(),
+class TestTimeSpecifiers:
+    @pytest.mark.parametrize(
+        ("format_", "expected"),
+        [
+            ("{h}", "04"),
+            ("{_h}", " 4"),
+            ("{^h}", "4"),
+            ("{h12}", "04"),
+            ("{_h12}", " 4"),
+            ("{^h12}", "4"),
+            ("{m}", "05"),
+            ("{_m}", " 5"),
+            ("{^m}", "5"),
+            ("{s}", "06"),
+            ("{_s}", " 6"),
+            ("{^s}", "6"),
+            ("{.f}", ".000007"),
+            ("{ms}", "000"),
+            ("{us}", "000007"),
+            ("{ns}", "000007000"),
+            ("{AM/PM}", "AM"),
+            ("{am/pm}", "am"),
+        ],
+        ids=[
+            "{h}",
+            "{_h}",
+            "{^h}",
+            "{h12}",
+            "{_h12}",
+            "{^h12}",
+            "{m}",
+            "{_m}",
+            "{^m}",
+            "{s}",
+            "{_s}",
+            "{^s}",
+            "{.f}",
+            "{ms}",
+            "{us}",
+            "{ns}",
+            "{AM/PM}",
+            "{am/pm}",
+        ],
     )
+    def test_should_be_replaced_with_correct_string(self, value: datetime | time, format_: str, expected: str) -> None:
+        assert_cell_operation_works(
+            value,
+            lambda cell: cell.dt.to_string(format=format_),
+            expected,
+        )
 
 
-def test_should_raise_for_unclosed_specifier():
+@pytest.mark.parametrize(
+    "value",
+    [
+        DATETIME,
+    ],
+    ids=[
+        "datetime",
+    ],
+)
+class TestDateTimeSpecifiers:
+    @pytest.mark.parametrize(
+        ("format_", "expected"),
+        [
+            ("{z}", "+0000"),
+            ("{:z}", "+00:00"),
+            ("{u}", "-62132730894"),
+        ],
+        ids=[
+            "{z}",
+            "{:z}",
+            "{u}",
+        ],
+    )
+    def test_should_be_replaced_with_correct_string(self, value: datetime, format_: str, expected: str) -> None:
+        assert_cell_operation_works(
+            value,
+            lambda cell: cell.dt.to_string(format=format_),
+            expected,
+        )
+
+
+def test_should_raise_for_unclosed_specifier() -> None:
     column = Column("a", [DATETIME])
     with pytest.raises(ValueError, match="Unclosed specifier"):
         column.transform(lambda cell: cell.dt.to_string(format="{Y"))
 
 
-def test_should_raise_for_globally_invalid_specifier():
+def test_should_raise_for_globally_invalid_specifier() -> None:
     column = Column("a", [DATETIME])
     with pytest.raises(ValueError, match="Invalid specifier"):
         column.transform(lambda cell: cell.dt.to_string(format="{invalid}"))
@@ -90,16 +221,21 @@ def test_should_raise_for_globally_invalid_specifier():
     ("value", "format_"),
     [
         (DATE, "{h}"),
-        (TIME, "{Y}"),
+        pytest.param(
+            TIME,
+            "{Y}",
+            marks=pytest.mark.skip("polars panics in this case (https://github.com/pola-rs/polars/issues/19853)."),
+        ),
     ],
     ids=[
         "invalid for date",
         "invalid for time",
     ],
 )
-def test_should_raise_for_specifier_that_is_invalid_for_type(value: date | time | None, format_: str):
+def test_should_raise_for_specifier_that_is_invalid_for_type(value: date | time | None, format_: str) -> None:
     # TODO: This is not the ideal behavior. Once https://github.com/Safe-DS/Library/issues/860 is resolved, we should
     #  do our own validation to raise an error that knows our own specifiers.
     column = Column("a", [value])
-    with pytest.raises((ComputeError, PanicException)):
-        column.transform(lambda cell: cell.dt.to_string(format=format_))
+    lazy_result = column.transform(lambda cell: cell.dt.to_string(format=format_))
+    with pytest.raises(LazyComputationError):
+        lazy_result.get_value(0)
