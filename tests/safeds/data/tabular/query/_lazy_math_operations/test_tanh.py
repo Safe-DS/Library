3fded103e74a23f0fe3ee77c664d8b4b82948ae7
Lars Reimann <mail@larsreimann.com>
2025-01-20 11:36:48 +0100

    test: `sinh`, `cosh`, `tanh`

---
+++ tests/safeds/data/tabular/query/_lazy_math_operations/test_tanh.py
@@ -0,0 +1,30 @@
+import math
+
+import pytest
+from helpers import assert_cell_operation_works
+
+from safeds.data.tabular.typing import ColumnType
+
+E = math.e
+PHI = (1 + math.sqrt(5)) / 2
+
+
+@pytest.mark.parametrize(
+    ("value", "expected"),
+    [
+        (0, 0),
+        (-1, -(E - 1 / E) / (E + 1 / E)),
+        (1, (E - 1 / E) / (E + 1 / E)),
+        (math.log(PHI), 1 / math.sqrt(5)),
+        (None, None),
+    ],
+    ids=[
+        "0",
+        "-1",
+        "1",
+        "ln of golden ratio",
+        "None",
+    ],
+)
+def test_should_return_hyperbolic_tangent(value: float | None, expected: float | None) -> None:
+    assert_cell_operation_works(value, lambda cell: cell.math.tanh(), expected, type_if_none=ColumnType.float64())
