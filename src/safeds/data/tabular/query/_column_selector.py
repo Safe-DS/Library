aa43e2ef1af3dc8d34cb6cb241cd4ea53b5872a9
Lars Reimann <mail@larsreimann.com>
2025-03-12 10:40:31 +0100

    docs: document new methods

--- src/safeds/data/tabular/query/_column_selector.py
+++ src/safeds/data/tabular/query/_column_selector.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 from abc import abstractmethod
 from typing import TYPE_CHECKING
 
+from safeds.data.tabular.typing import ColumnType
+
 if TYPE_CHECKING:
     import polars as pl
-
-    from safeds.data.tabular.typing import ColumnType
 
 
 class ColumnSelector:
@@ -16,6 +16,30 @@ class ColumnSelector:
 
     @staticmethod
     def all() -> ColumnSelector:
+        """
+        Select all columns.
+
+        Returns
+        -------
+        selector:
+            The selector.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"a": [1, 2, 3], "b": ["a", "b", "c"]})
+        >>> table.select_columns(ColumnSelector.all())
+        +-----+-----+
+        |   a | b   |
+        | --- | --- |
+        | i64 | str |
+        +===========+
+        |   1 | a   |
+        |   2 | b   |
+        |   3 | c   |
+        +-----+-----+
+        """
         import polars.selectors as cs
 
         from ._lazy_column_selector import _LazyColumnSelector  # circular import
@@ -24,6 +48,35 @@ class ColumnSelector:
 
     @staticmethod
     def by_index(indices: int | list[int]) -> ColumnSelector:
+        """
+        Select columns by their index.
+
+        Parameters
+        ----------
+        indices:
+            The indices to select.
+
+        Returns
+        -------
+        selector:
+            The selector.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"a": [1, 2, 3], "b": ["a", "b", "c"]})
+        >>> table.select_columns(ColumnSelector.by_index(0))
+        +-----+
+        |   a |
+        | --- |
+        | i64 |
+        +=====+
+        |   1 |
+        |   2 |
+        |   3 |
+        +-----+
+        """
         import polars.selectors as cs
 
         from ._lazy_column_selector import _LazyColumnSelector  # circular import
@@ -32,6 +85,38 @@ class ColumnSelector:
 
     @staticmethod
     def by_name(names: str | list[str], *, ignore_unknown_names: bool = False) -> ColumnSelector:
+        """
+        Select columns by their name.
+
+        Parameters
+        ----------
+        names:
+            The names to select.
+        ignore_unknown_names:
+            If set to True, columns that are not present in the table will be ignored.
+            If set to False, an error will be raised if any of the specified columns do not exist.
+
+        Returns
+        -------
+        selector:
+            The selector.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"a": [1, 2, 3], "b": ["a", "b", "c"]})
+        >>> table.select_columns(ColumnSelector.by_name("a"))
+        +-----+
+        |   a |
+        | --- |
+        | i64 |
+        +=====+
+        |   1 |
+        |   2 |
+        |   3 |
+        +-----+
+        """
         import polars.selectors as cs
 
         from ._lazy_column_selector import _LazyColumnSelector  # circular import
@@ -40,14 +125,73 @@ class ColumnSelector:
 
     @staticmethod
     def by_type(types: ColumnType | list[ColumnType]) -> ColumnSelector:
+        """
+        Select all columns that have a specific type.
+
+        Parameters
+        ----------
+        types:
+            The column types to select.
+
+        Returns
+        -------
+        selector:
+            The selector.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> from safeds.data.tabular.typing import ColumnType
+        >>> table = Table({"a": [1, 2, 3], "b": ["a", "b", "c"]})
+        >>> table.select_columns(ColumnSelector.by_type(ColumnType.string()))
+        +-----+
+        | b   |
+        | --- |
+        | str |
+        +=====+
+        | a   |
+        | b   |
+        | c   |
+        +-----+
+        """
         import polars.selectors as cs
 
         from ._lazy_column_selector import _LazyColumnSelector  # circular import
+
+        if isinstance(types, ColumnType):
+            types = [types]
+
+        types = [t._polars_data_type for t in types]
 
         return _LazyColumnSelector(cs.by_dtype(types))
 
     @staticmethod
     def is_float() -> ColumnSelector:
+        """
+        Select all columns that have a floating point type.
+
+        Returns
+        -------
+        selector:
+            The selector.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"a": [1.0, 2.0, 3.0], "b": ["a", "b", "c"]})
+        >>> table.select_columns(ColumnSelector.is_float())
+        +---------+
+        |       a |
+        |     --- |
+        |     f64 |
+        +=========+
+        | 1.00000 |
+        | 2.00000 |
+        | 3.00000 |
+        +---------+
+        """
         import polars.selectors as cs
 
         from ._lazy_column_selector import _LazyColumnSelector  # circular import
@@ -56,6 +200,30 @@ class ColumnSelector:
 
     @staticmethod
     def is_int() -> ColumnSelector:
+        """
+        Select all columns that have an integer type.
+
+        Returns
+        -------
+        selector:
+            The selector.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"a": [1, 2, 3], "b": ["a", "b", "c"]})
+        >>> table.select_columns(ColumnSelector.is_int())
+        +-----+
+        |   a |
+        | --- |
+        | i64 |
+        +=====+
+        |   1 |
+        |   2 |
+        |   3 |
+        +-----+
+        """
         import polars.selectors as cs
 
         from ._lazy_column_selector import _LazyColumnSelector  # circular import
@@ -64,6 +232,30 @@ class ColumnSelector:
 
     @staticmethod
     def is_numeric() -> ColumnSelector:
+        """
+        Select all columns that have a numeric type.
+
+        Returns
+        -------
+        selector:
+            The selector.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"a": [1, 2, 3], "b": ["a", "b", "c"]})
+        >>> table.select_columns(ColumnSelector.is_numeric())
+        +-----+
+        |   a |
+        | --- |
+        | i64 |
+        +=====+
+        |   1 |
+        |   2 |
+        |   3 |
+        +-----+
+        """
         import polars.selectors as cs
 
         from ._lazy_column_selector import _LazyColumnSelector  # circular import
@@ -72,6 +264,30 @@ class ColumnSelector:
 
     @staticmethod
     def is_signed_int() -> ColumnSelector:
+        """
+        Select all columns that have a signed integer type.
+
+        Returns
+        -------
+        selector:
+            The selector.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"a": [1, 2, 3], "b": ["a", "b", "c"]})
+        >>> table.select_columns(ColumnSelector.is_signed_int())
+        +-----+
+        |   a |
+        | --- |
+        | i64 |
+        +=====+
+        |   1 |
+        |   2 |
+        |   3 |
+        +-----+
+        """
         import polars.selectors as cs
 
         from ._lazy_column_selector import _LazyColumnSelector  # circular import
@@ -80,6 +296,34 @@ class ColumnSelector:
 
     @staticmethod
     def is_temporal() -> ColumnSelector:
+        """
+        Select all columns that have a temporal type.
+
+        Returns
+        -------
+        selector:
+            The selector.
+
+        Examples
+        --------
+        >>> from datetime import date
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({
+        ...     "a": [date(2022, 1, 1), date(2022, 1, 2), date(2022, 1, 3)],
+        ...     "b": ["a", "b", "c"]
+        ... })
+        >>> table.select_columns(ColumnSelector.is_temporal())
+        +------------+
+        | a          |
+        | ---        |
+        | date       |
+        +============+
+        | 2022-01-01 |
+        | 2022-01-02 |
+        | 2022-01-03 |
+        +------------+
+        """
         import polars.selectors as cs
 
         from ._lazy_column_selector import _LazyColumnSelector  # circular import
@@ -88,6 +332,32 @@ class ColumnSelector:
 
     @staticmethod
     def is_unsigned_int() -> ColumnSelector:
+        """
+        Select all columns that have a unsigned integer type.
+
+        Returns
+        -------
+        selector:
+            The selector.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Column, Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"b": ["a", "b", "c"]})
+        >>> column = Column("a", [1, 2, 3], type=ColumnType.uint8())
+        >>> table = table.add_columns(column)
+        >>> table.select_columns(ColumnSelector.is_unsigned_int())
+        +-----+
+        |   a |
+        | --- |
+        |  u8 |
+        +=====+
+        |   1 |
+        |   2 |
+        |   3 |
+        +-----+
+        """
         import polars.selectors as cs
 
         from ._lazy_column_selector import _LazyColumnSelector  # circular import
@@ -147,18 +417,232 @@ class ColumnSelector:
     # ------------------------------------------------------------------------------------------------------------------
 
     def not_(self) -> ColumnSelector:
+        """
+        Invert the selector. This is equivalent to the `~` operator.
+
+        Do **not** use the `not` operator. Its behavior cannot be overwritten in Python, so it will not work as
+        expected.
+
+        Returns
+        -------
+        selector:
+            The inverted selector.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"a": [1, 2, 3], "b": ["a", "b", "c"]})
+        >>> table.select_columns(ColumnSelector.is_numeric().not_())
+        +-----+
+        | b   |
+        | --- |
+        | str |
+        +=====+
+        | a   |
+        | b   |
+        | c   |
+        +-----+
+
+        >>> table.select_columns(~ColumnSelector.is_numeric())
+        +-----+
+        | b   |
+        | --- |
+        | str |
+        +=====+
+        | a   |
+        | b   |
+        | c   |
+        +-----+
+        """
         return self.__invert__()
 
     def and_(self, other: ColumnSelector) -> ColumnSelector:
+        """
+        Create the intersection of two selectors. This is equivalent to the `&` operator.
+
+        Parameters
+        ----------
+        other:
+            The other selector.
+
+        Returns
+        -------
+        selector:
+            The intersection of the two selectors.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"a": [1, 2, 3], "b": ["a", "b", "c"]})
+        >>> table.select_columns(
+        ...     ColumnSelector.is_numeric().and_(ColumnSelector.by_name("a"))
+        ... )
+        +-----+
+        |   a |
+        | --- |
+        | i64 |
+        +=====+
+        |   1 |
+        |   2 |
+        |   3 |
+        +-----+
+
+        >>> table.select_columns(
+        ...     ColumnSelector.is_numeric() & ColumnSelector.by_name("a")
+        ... )
+        +-----+
+        |   a |
+        | --- |
+        | i64 |
+        +=====+
+        |   1 |
+        |   2 |
+        |   3 |
+        +-----+
+        """
         return self.__and__(other)
 
     def or_(self, other: ColumnSelector) -> ColumnSelector:
+        """
+        Create the union of two selectors. This is equivalent to the `|` operator.
+
+        Parameters
+        ----------
+        other:
+            The other selector.
+
+        Returns
+        -------
+        selector:
+            The union of the two selectors.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"a": [1, 2, 3], "b": ["a", "b", "c"]})
+        >>> table.select_columns(
+        ...     ColumnSelector.is_numeric().or_(ColumnSelector.by_name("b"))
+        ... )
+        +-----+-----+
+        |   a | b   |
+        | --- | --- |
+        | i64 | str |
+        +===========+
+        |   1 | a   |
+        |   2 | b   |
+        |   3 | c   |
+        +-----+-----+
+
+        >>> table.select_columns(
+        ...     ColumnSelector.is_numeric() | ColumnSelector.by_name("b")
+        ... )
+        +-----+-----+
+        |   a | b   |
+        | --- | --- |
+        | i64 | str |
+        +===========+
+        |   1 | a   |
+        |   2 | b   |
+        |   3 | c   |
+        +-----+-----+
+        """
         return self.__or__(other)
 
     def xor(self, other: ColumnSelector) -> ColumnSelector:
+        """
+        Create the symmetric difference of two selectors. This is equivalent to the `^` operator.
+
+        Parameters
+        ----------
+        other:
+            The other selector.
+
+        Returns
+        -------
+        selector:
+            The symmetric difference of the two selectors.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"a": [1, 2, 3], "b": ["a", "b", "c"]})
+        >>> table.select_columns(
+        ...     ColumnSelector.is_numeric().xor(ColumnSelector.by_name("b"))
+        ... )
+        +-----+-----+
+        |   a | b   |
+        | --- | --- |
+        | i64 | str |
+        +===========+
+        |   1 | a   |
+        |   2 | b   |
+        |   3 | c   |
+        +-----+-----+
+
+        >>> table.select_columns(
+        ...     ColumnSelector.is_numeric() ^ ColumnSelector.by_name("b")
+        ... )
+        +-----+-----+
+        |   a | b   |
+        | --- | --- |
+        | i64 | str |
+        +===========+
+        |   1 | a   |
+        |   2 | b   |
+        |   3 | c   |
+        +-----+-----+
+        """
         return self.__xor__(other)
 
     def sub(self, other: ColumnSelector) -> ColumnSelector:
+        """
+        Create the difference of two selectors. This is equivalent to the `-` operator.
+
+        Parameters
+        ----------
+        other:
+            The other selector.
+
+        Returns
+        -------
+        selector:
+            The difference of the two selectors.
+
+        Examples
+        --------
+        >>> from safeds.data.tabular.containers import Table
+        >>> from safeds.data.tabular.query import ColumnSelector
+        >>> table = Table({"a": [1, 2, 3], "b": ["a", "b", "c"]})
+        >>> table.select_columns(
+        ...     ColumnSelector.is_numeric().sub(ColumnSelector.by_name("b"))
+        ... )
+        +-----+
+        |   a |
+        | --- |
+        | i64 |
+        +=====+
+        |   1 |
+        |   2 |
+        |   3 |
+        +-----+
+
+        >>> table.select_columns(
+        ...     ColumnSelector.is_numeric() - ColumnSelector.by_name("b")
+        ... )
+        +-----+
+        |   a |
+        | --- |
+        | i64 |
+        +=====+
+        |   1 |
+        |   2 |
+        |   3 |
+        +-----+
+        """
         return self.__sub__(other)
 
     # ------------------------------------------------------------------------------------------------------------------
